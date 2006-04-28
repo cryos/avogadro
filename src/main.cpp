93a548b81ed1592b30fd0c95f7309e0c897573bd
Donald Ephraim Curtis <dcurtis3@sourceforge.net>
2006-04-28 09:26:19 +0000

    Tutorial time.
    git-svn-id: https://avogadro.svn.sourceforge.net/svnroot/avogadro/trunk@5 af78ad9f-9711-0410-ba57-a8eff4914ec4

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
-#include "AWindow.h"
 #include <QApplication>
+#include <QPushButton>
 
 int main(int argc, char ** argv)
 {
 	QApplication app(argc, argv);
 	printf("Starting Avogadro.\n");
+
+	QPushButton hello("Hello Avogadro");
+	hello.resize(100,30);
+	hello.show();
 
 	return app.exec();
 }
