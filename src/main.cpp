e48caad24a633b31cf5d3696557f97a9ef159e9e
Donald Ephraim Curtis <dcurtis3@sourceforge.net>
2006-08-20 21:52:11 +0000

    The 'foreach' command does not ignore the first element in the list.  Therefore it was trying to load "./avogadro" everytime it loaded.  Reverted back to old way; this isn't wrong it's just not as "pretty" but it's a specific issue.
    git-svn-id: https://avogadro.svn.sourceforge.net/svnroot/avogadro/trunk@25 af78ad9f-9711-0410-ba57-a8eff4914ec4

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,24 @@
 /**********************************************************************
-main.cpp - main program, initialization and launching
+  main.cpp - main program, initialization and launching
 
-Copyright (C) 2006 by Geoffrey R. Hutchison
-Some portions Copyright (C) 2006 by Donald E. Curtis
+  Copyright (C) 2006 by Geoffrey R. Hutchison
+  Some portions Copyright (C) 2006 by Donald E. Curtis
 
-This file is part of the Avogadro molecular editor project.
-For more information, see <http://avogadro.sourceforge.net/>
+  This file is part of the Avogadro molecular editor project.
+  For more information, see <http://avogadro.sourceforge.net/>
 
-Some code is based on Open Babel
-For more information, see <http://openbabel.sourceforge.net/>
+  Some code is based on Open Babel
+  For more information, see <http://openbabel.sourceforge.net/>
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation version 2 of the License.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation version 2 of the License.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-***********************************************************************/
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+ ***********************************************************************/
 
 // QT Includes
 #include <QApplication>
@@ -45,17 +45,18 @@ int main(int argc, char *argv[])
   //  app.setQuitOnLastWindowClosed(false); // remain open until quit()
 
   QStringList files = app.arguments();
-  
+
   if (files.size() > 1) {
     QPoint p(100, 100), offset(40,40);
-    foreach(QString s, files )
-      {
-        MainWindow *other = new MainWindow;
-        p += offset;
-        other->move(p);
-        other->show();
-        other->loadFile(s);
-      }
+    QList<QString>::const_iterator i = files.constBegin();
+    for (++i; i != files.constEnd(); ++i)
+    {
+      MainWindow *other = new MainWindow;
+      p += offset;
+      other->move(p);
+      other->show();
+      other->loadFile(*i);
+    }
   } else {
     MainWindow *window = new MainWindow;
     window->show();
