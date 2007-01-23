57dc510a131a12f77bd896f8591b7d7074610821
Donald Ephraim Curtis <dcurtis3@sourceforge.net>
2007-01-23 18:39:23 +0000

    Updated FindEigen.cmake from pinotree.
    git-svn-id: https://avogadro.svn.sourceforge.net/svnroot/avogadro/trunk@103 af78ad9f-9711-0410-ba57-a8eff4914ec4

--- cmake/modules/FindEigen.cmake
+++ cmake/modules/FindEigen.cmake
@@ -3,21 +3,18 @@
 #
 #  EIGEN_FOUND - system has eigen lib
 #  EIGEN_INCLUDE_DIR - the eigen include directory
-#  EIGEN_LIBRARIES - Link these to use eigen lib
 #
 # Copyright (c) 2006, Montel Laurent, <montel@kde.org>
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
+if (EIGEN_INCLUDE_DIR)
 
+  # in cache already
+  set(EIGEN_FOUND TRUE)
 
-find_path(EIGEN_LIBRARIES NAMES libeigen.a
-             PATHS
-	     ${LIB_INSTALL_DIR}/
-             /usr/lib/
-             /usr/local/lib/
-   )
-message(STATUS "EIGEN_LIBRARIES :<${EIGEN_LIBRARIES}>")
+else (EIGEN_INCLUDE_DIR)
+
 find_path(EIGEN_INCLUDE_DIR NAMES eigen/matrix.h
      PATHS
      ${INCLUDE_INSTALL_DIR}
@@ -25,20 +22,21 @@ find_path(EIGEN_INCLUDE_DIR NAMES eigen/matrix.h
      /usr/local/include
    )
 
-   message(STATUS "EIGEN_INCLUDE_DIR <${EIGEN_INCLUDE_DIR}>")
-if(EIGEN_INCLUDE_DIR AND EIGEN_LIBRARIES)
+if(EIGEN_INCLUDE_DIR)
   set(EIGEN_FOUND TRUE)
-endif(EIGEN_INCLUDE_DIR AND EIGEN_LIBRARIES)
+endif(EIGEN_INCLUDE_DIR)
 
 if(EIGEN_FOUND)
-   if(NOT eigen_FIND_QUIETLY)
-      message(STATUS "Found eigen: ${EIGEN_LIBRARIES}")
-   endif(NOT eigen_FIND_QUIETLY)
+   if(NOT Eigen_FIND_QUIETLY)
+      message(STATUS "Found Eigen: ${EIGEN_INCLUDE_DIR}")
+   endif(NOT Eigen_FIND_QUIETLY)
 else(EIGEN_FOUND)
-   if(eigen_FIND_REQUIRED)
-      message(FATAL_ERROR "Could not find eigen")
-   endif(eigen_FIND_REQUIRED)
+   if(Eigen_FIND_REQUIRED)
+      message(FATAL_ERROR "Could NOT find Eigen")
+   endif(Eigen_FIND_REQUIRED)
 endif(EIGEN_FOUND)
 
-mark_as_advanced(EIGEN_INCLUDE_DIR EIGEN_LIBRARIES )
- 
+mark_as_advanced(EIGEN_INCLUDE_DIR)
+
+endif(EIGEN_INCLUDE_DIR)
+
