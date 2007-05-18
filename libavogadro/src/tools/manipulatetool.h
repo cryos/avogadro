37f68cc5f82dd30b191bf09a15c5e30a22717c72
Carsten Niehaus <carsten.niehaus@gmail.com>
2007-05-18 12:23:12 +0000

    Forgot two places
    git-svn-id: https://avogadro.svn.sourceforge.net/svnroot/avogadro/trunk@423 af78ad9f-9711-0410-ba57-a8eff4914ec4

--- libavogadro/src/tools/manipulatetool.h
+++ libavogadro/src/tools/manipulatetool.h
@@ -101,8 +101,8 @@ namespace Avogadro {
 
   class ManipulateToolFactory : public QObject, public ToolFactory
     {
-      Q_OBJECT;
-      Q_INTERFACES(Avogadro::ToolFactory);
+      Q_OBJECT
+      Q_INTERFACES(Avogadro::ToolFactory)
 
       public:
         Tool *createInstance(QObject *parent = 0) { return new ManipulateTool(); }
