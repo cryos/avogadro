4c32aae1e19dccf59340b18397a7920a91c617d1
Carsten Niehaus <carsten.niehaus@gmail.com>
2007-05-18 18:36:08 +0000

    Adding a nice little script which runs krazy.
    git-svn-id: https://avogadro.svn.sourceforge.net/svnroot/avogadro/trunk@430 af78ad9f-9711-0410-ba57-a8eff4914ec4

---
+++ krazy/runkrazy.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+# Small Ruby-script to run Krazy over all directories
+#
+# (c) 2007 Carsten Niehaus <cniehaus at kde dot org>
+#
+# License: GPL V2
+
+#you need to run a command like this:
+# export PATH=$PATH:/usr/local/Krazy/bin
+
+def dir2filename( dir )
+	dir = dir.gsub( "/", "-" )
+	dir
+end
+
+
+directories = [ "libavogadro" , "libavogadro/src" , "libavogadro/src/tools", "libavogadro/src/engines" , "avogadro/src" , "avogadro/src/extensions" ]
+
+for dir in directories
+	dir.chomp!
+	filename = dir2filename( dir )
+
+	command = "krazy --export html --explain ../#{dir}/* > #{filename}.html"
+
+	puts "Running Krazy on #{dir}".center(70, "-")
+	puts "Results are written in #{filename}.html".center(70, "-")
+	`#{command}`
+end
+
