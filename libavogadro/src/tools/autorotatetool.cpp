eb2f93f2d93e0f798cb111bcc5a97de72c316f7e
Marcus D. Hanwell <marcus@cryos.org>
2007-06-13 08:16:19 +0000

    Added new auto rotation tool. The rotation speed about the x, y and z axes can be set using sliders. It needs an icon.
    git-svn-id: https://avogadro.svn.sourceforge.net/svnroot/avogadro/trunk@495 af78ad9f-9711-0410-ba57-a8eff4914ec4

---
+++ libavogadro/src/tools/autorotatetool.cpp
@@ -0,0 +1,185 @@
+/**********************************************************************
+  AutoRotateTool - Auto Rotation Tool for Avogadro
+
+  Copyright (C) 2007 by Marcus D. Hanwell
+
+  This file is part of the Avogadro molecular editor project.
+  For more information, see <http://avogadro.sourceforge.net/>
+
+  Avogadro is free software; you can redistribute it and/or modify 
+  it under the terms of the GNU General Public License as published by 
+  the Free Software Foundation; either version 2 of the License, or 
+  (at your option) any later version.
+
+  Avogadro is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+  02110-1301, USA.
+ **********************************************************************/
+
+#include "autorotatetool.h"
+#include <avogadro/primitive.h>
+#include <avogadro/glwidget.h>
+#include <avogadro/camera.h>
+
+#include <QtPlugin>
+#include <QObject>
+#include <QSlider>
+#include <QLabel>
+#include <QVBoxLayout>
+
+using namespace std;
+using namespace OpenBabel;
+using namespace Avogadro;
+using namespace Eigen;
+
+AutoRotateTool::AutoRotateTool(QObject *parent) : Tool(parent), m_glwidget(0), 
+  m_xRotation(0), m_yRotation(0), m_zRotation(0), m_settingsWidget(0)
+{
+  QAction *action = activateAction();
+  action->setIcon(QIcon(QString::fromUtf8(":/navigate/navigate.png")));
+  action->setToolTip(tr("Auto Rotation Tool"));
+
+  // Start a timer for auto rotation
+  startTimer(25);
+}
+
+AutoRotateTool::~AutoRotateTool()
+{
+}
+
+int AutoRotateTool::usefulness() const
+{
+  return 20000;
+}
+
+void AutoRotateTool::rotate() const
+{
+  // Get back transformed axes that we can rotate around
+  Vector3d xAxis = m_glwidget->camera()->backtransformedXAxis();
+  Vector3d yAxis = m_glwidget->camera()->backtransformedYAxis();
+  Vector3d zAxis = m_glwidget->camera()->backtransformedZAxis();
+  // Perform the rotations
+  m_glwidget->camera()->translate( m_glwidget->center() );
+  m_glwidget->camera()->rotate( m_xRotation * ROTATION_SPEED, yAxis );
+  m_glwidget->camera()->rotate( m_yRotation * ROTATION_SPEED, xAxis );
+  m_glwidget->camera()->rotate( m_zRotation * ROTATION_SPEED, zAxis );
+  m_glwidget->camera()->translate( -m_glwidget->center() );
+}
+
+bool AutoRotateTool::paint(GLWidget *widget)
+{
+  // Get the widget for our rotation function
+  m_glwidget = widget;
+  return true;
+}
+
+void AutoRotateTool::timerEvent(QTimerEvent*)
+{
+  // If there is a glwidget and at least one axis is set for rotation
+  // then perform rotation and trigger a window update
+  if (m_glwidget && (m_xRotation || m_yRotation || m_zRotation))
+  {
+    rotate();
+    m_glwidget->update();
+  }
+}
+
+void AutoRotateTool::setXRotation(int i)
+{
+  m_xRotation = i;
+}
+
+void AutoRotateTool::setYRotation(int i)
+{
+  m_yRotation = i;
+}
+
+void AutoRotateTool::setZRotation(int i)
+{
+  m_zRotation = i;
+}
+
+QWidget* AutoRotateTool::settingsWidget() {
+  if(!m_settingsWidget) {
+    m_settingsWidget = new QWidget;
+
+    // Label and slider to set x axis rotation
+    QLabel* labelX = new QLabel("x rotation:");
+    labelX->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+    labelX->setMaximumHeight(15);
+    QSlider* sliderXRotation = new QSlider(m_settingsWidget);
+    sliderXRotation->setOrientation(Qt::Horizontal);
+    sliderXRotation->setTickPosition(QSlider::TicksBothSides);
+    sliderXRotation->setToolTip("x rotation");
+    sliderXRotation->setTickInterval(5);
+    sliderXRotation->setPageStep(5);
+    sliderXRotation->setMinimum(0);
+    sliderXRotation->setMaximum(20);
+    sliderXRotation->setValue(0);
+
+    // Label and slider to set y axis rotation
+    QLabel* labelY = new QLabel("y rotation:");
+    labelY->setMaximumHeight(15);
+    QSlider* sliderYRotation = new QSlider(m_settingsWidget);
+    sliderYRotation->setOrientation(Qt::Horizontal);
+    sliderYRotation->setTickPosition(QSlider::TicksBothSides);
+    sliderYRotation->setToolTip("y rotation");
+    sliderYRotation->setTickInterval(5);
+    sliderYRotation->setPageStep(5);
+    sliderYRotation->setMinimum(0);
+    sliderYRotation->setMaximum(20);
+    sliderYRotation->setValue(0);
+
+    // Label and slider to set z axis rotation
+    QLabel* labelZ = new QLabel("z rotation:");
+    labelZ->setMaximumHeight(15);
+    QSlider* sliderZRotation = new QSlider(m_settingsWidget);
+    sliderZRotation->setOrientation(Qt::Horizontal);
+    sliderZRotation->setTickPosition(QSlider::TicksBothSides);
+    sliderZRotation->setToolTip("z rotation");
+    sliderZRotation->setTickInterval(5);
+    sliderZRotation->setPageStep(5);
+    sliderZRotation->setMinimum(0);
+    sliderZRotation->setMaximum(20);
+    sliderZRotation->setValue(0);
+
+    QVBoxLayout* layout = new QVBoxLayout();
+    layout->addWidget(labelX);
+    layout->addWidget(sliderXRotation);
+    layout->addWidget(labelY);
+    layout->addWidget(sliderYRotation);
+    layout->addWidget(labelZ);
+    layout->addWidget(sliderZRotation);
+    m_settingsWidget->setLayout(layout);
+
+    // Connect the sliders with the slots
+    connect(sliderXRotation, SIGNAL(valueChanged(int)),
+        this, SLOT(setXRotation(int)));
+
+    connect(sliderYRotation, SIGNAL(valueChanged(int)),
+        this, SLOT(setYRotation(int)));
+
+    connect(sliderZRotation, SIGNAL(valueChanged(int)),
+        this, SLOT(setZRotation(int)));
+
+    connect(m_settingsWidget, SIGNAL(destroyed()),
+        this, SLOT(settingsWidgetDestroyed()));
+  }
+
+  return m_settingsWidget;
+}
+
+void AutoRotateTool::settingsWidgetDestroyed()
+{
+  m_settingsWidget = 0;
+}
+
+#include "autorotatetool.moc"
+
+Q_EXPORT_PLUGIN2(autorotatetool, AutoRotateToolFactory)
---
+++ libavogadro/src/tools/autorotatetool.h
@@ -0,0 +1,113 @@
+/**********************************************************************
+  AutoRotateTool - Auto Rotation Tool for Avogadro
+
+  Copyright (C) 2007 by Marcus D. Hanwell
+
+  This file is part of the Avogadro molecular editor project.
+  For more information, see <http://avogadro.sourceforge.net/>
+
+  Avogadro is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  Avogadro is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+  02110-1301, USA.
+ **********************************************************************/
+
+#ifndef __AUTOROTATETOOL_H
+#define __AUTOROTATETOOL_H
+
+#include <avogadro/glwidget.h>
+#include <avogadro/tool.h>
+
+#include <QGLWidget>
+#include <QObject>
+#include <QStringList>
+#include <QImage>
+#include <QAction>
+
+namespace Avogadro {
+
+  /**
+   * @class AutoRotateTool
+   * @brief Automatic molecule rotation
+   * @author Marcus D. Hanwell
+   *
+   * This class handles the automatic rotation of the view
+   * so that the molecule can be shown in more detail.
+   */
+  class AutoRotateTool : public Tool
+  {
+    Q_OBJECT
+
+    public:
+      //! Constructor
+      AutoRotateTool(QObject *parent = 0);
+      //! Destructor
+      virtual ~AutoRotateTool();
+
+      //! \name Description methods
+      //@{
+      //! Tool Name
+      virtual QString name() const { return(tr("AutoRotate")); }
+      //! Tool Description
+      virtual QString description() const { return(tr("Auto Rotation Tool")); }
+      //@}
+
+      //! \name Tool Methods
+      //@{
+      //! \brief Callback methods for ui.actions on the canvas.
+      /*!
+      */
+      virtual QUndoCommand* mousePress(GLWidget*, const QMouseEvent*) { return 0; }
+      virtual QUndoCommand* mouseRelease(GLWidget*, const QMouseEvent*) { return 0; }
+      virtual QUndoCommand* mouseMove(GLWidget*, const QMouseEvent*) { return 0; }
+      virtual QUndoCommand* wheel(GLWidget*, const QWheelEvent*) { return 0; }
+
+      virtual int usefulness() const;
+
+      virtual bool paint(GLWidget *widget);
+
+
+      virtual QWidget* settingsWidget();
+
+    public Q_SLOTS:
+      void setXRotation(int i);
+      void setYRotation(int i);
+      void setZRotation(int i);
+
+    protected:
+      GLWidget* m_glwidget;
+      int m_xRotation;
+      int m_yRotation;
+      int m_zRotation;
+
+      void rotate() const;
+      void timerEvent(QTimerEvent* event);
+
+      QWidget* m_settingsWidget;
+
+    private Q_SLOTS:
+      void settingsWidgetDestroyed();
+  };
+
+  class AutoRotateToolFactory : public QObject, public ToolFactory
+    {
+      Q_OBJECT
+      Q_INTERFACES(Avogadro::ToolFactory)
+
+      public:
+        Tool* createInstance(QObject *parent = 0) { return new AutoRotateTool(parent); }
+    };
+
+} // end namespace Avogadro
+
+#endif
