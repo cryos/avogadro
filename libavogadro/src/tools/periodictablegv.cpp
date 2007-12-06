0b620e3cdef653cc0c384f27a3bc125515a67c24
Marcus D. Hanwell <marcus@cryos.org>
2007-12-06 04:42:49 +0000

    More tweaking of the ElementDetail painting.
    git-svn-id: https://avogadro.svn.sourceforge.net/svnroot/avogadro/trunk@924 af78ad9f-9711-0410-ba57-a8eff4914ec4

--- libavogadro/src/tools/periodictablegv.cpp
+++ libavogadro/src/tools/periodictablegv.cpp
@@ -1,26 +1,26 @@
 /**********************************************************************
  PeriodicTableGV - Periodic Table Graphics View for Avogadro
- 
+
  Copyright (C) 2007 by Marcus D. Hanwell
- 
+
  This file is part of the Avogadro molecular editor project.
  For more information, see <http://avogadro.sourceforge.net/>
- 
+
  Avogadro is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation; either version 2 of the License, or
  (at your option) any later version.
- 
+
  Avogadro is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
- 
+
  You should have received a copy of the GNU General Public License
  along with this program; if not, write to the Free Software
  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  02110-1301, USA.
- 
+
  **********************************************************************/
 
 #include "periodictablegv.h"
@@ -46,7 +46,7 @@ namespace Avogadro {
   {
     // Set up some reasonable defaults for our item
     m_element = elementNumber;
-    
+
     // Want these items to be selectable
     setFlags(QGraphicsItem::ItemIsSelectable);
 
@@ -57,7 +57,6 @@ namespace Avogadro {
     // Set some custom data to make it easy to figure out which element we are
     setData(0, m_element);
     setData(1, 8 + m_element);
-    qDebug() << m_symbol << " (" << m_element << "): " << color[0] << ", " << color[1] << ", " << color[2];
   }
 
   QRectF ElementItem::boundingRect() const
@@ -75,14 +74,23 @@ namespace Avogadro {
   void ElementItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *,
                           QWidget *)
   {
+    // Fill the rectangle with the element colour
     painter->setBrush(*m_color);
-    if (isSelected()) {
-      qDebug() << "Element " << m_symbol << " selected.";
-      painter->setPen(Qt::white);
+
+    // Handle the case where the item is selected
+    if (m_element == 1 || m_element == 2 || m_element == 9 || m_element == 47 || m_element == 78) {
+      if (isSelected())
+        painter->setPen(Qt::gray);
+      else
+        painter->setPen(Qt::black);
     }
     else {
-      painter->setPen(Qt::black);
+      if (isSelected())
+        painter->setPen(Qt::white);
+      else
+        painter->setPen(Qt::black);
     }
+
     QRectF rect(-m_width/2, -m_height/2, m_width, m_height);
     painter->drawRect(rect);
     painter->drawText(rect, Qt::AlignCenter, m_symbol);
@@ -93,19 +101,19 @@ namespace Avogadro {
     // Set up some reasonable defaults for our item
     m_element = elementNumber;
   }
-  
+
   QRectF ElementDetail::boundingRect() const
   {
     return QRectF(-m_width/2, -m_height/2, m_width, m_height);
   }
-  
+
   QPainterPath ElementDetail::shape() const
   {
     QPainterPath path;
     path.addRect(-m_width/2, -m_height/2, m_width, m_height);
     return path;
   }
-  
+
   void ElementDetail::paint(QPainter *painter, const QStyleOptionGraphicsItem *,
                           QWidget *)
   {
@@ -115,39 +123,44 @@ namespace Avogadro {
     painter->setFont(font);
     QFontMetrics fm(font);
     int pixelHeight = fm.height();
-    
+
     QString symbol = OpenBabel::etab.GetSymbol(m_element);
     QString name = OpenBabel::etab.GetName(m_element).c_str();
     QString mass = QString::number(OpenBabel::etab.GetMass(m_element), 'f', 3);
-    
+
     std::vector<double> color = OpenBabel::etab.GetRGB(m_element);
     QColor m_color;
     m_color.setRgbF(color[0], color[1], color[2]);
-    
+
     painter->setBrush(m_color);
     painter->setPen(Qt::black);
     QRectF rect(-m_width/2, -m_height/2, m_width, m_height);
     painter->drawRect(rect);
 
-    // Draw the element symbol in the centre of the item
+    // Draw the element symbol bigger than everything else
     font.setPixelSize(24);
     QFontMetrics fm2(font);
     pixelHeight = fm2.height();
     int pixelWidth = fm2.width(symbol);
     painter->setFont(font);
-    QRectF symbolRect(-10, -m_height/2 + 10, pixelWidth, pixelHeight);
+    QRectF symbolRect(-10, -m_height/2 + 8, pixelWidth, pixelHeight);
     painter->drawText(symbolRect, Qt::AlignCenter, symbol);
 
+    // Reduce the font size to draw the other parts
     font.setPixelSize(12);
-    pixelHeight = fm.height();
+    int pixelHeight2 = fm.height();
     painter->setFont(font);
-    QRectF protonNumberRect(-m_width/2 - 10, -m_height/2 + 10, m_width/2, pixelHeight);
+
+    // Draw the proton number
+    QRectF protonNumberRect(-m_width/2 - 10, -m_height/2 + 8, m_width/2, pixelHeight2);
     painter->drawText(protonNumberRect, Qt::AlignRight, QString::number(m_element));
-    
-    QRectF massNumberRect(-m_width/2, pixelHeight*0.5, m_width, pixelHeight);
+
+    // Draw the mass
+    QRectF massNumberRect(-m_width/2, -m_height/2 + 8 + pixelHeight*1.1, m_width, pixelHeight2);
     painter->drawText(massNumberRect, Qt::AlignCenter, mass);
-    
-    QRectF nameRect(-m_width/2, pixelHeight*1.5, m_width, pixelHeight);
+
+    // Finally the full element name
+    QRectF nameRect(-m_width/2, -m_height/2 + 4 + pixelHeight*1.1 + pixelHeight2, m_width, pixelHeight);
     painter->drawText(nameRect, Qt::AlignCenter, name);
   }
 
@@ -170,19 +183,19 @@ namespace Avogadro {
     // Connect the slot and the signal...
     connect(this, SIGNAL(elementChanged(int)),
             detail, SLOT(elementChanged(int)));
-    
+
     ElementItem *item = new ElementItem(1);
     item->setPos( 0 * width, 0 * height);
     addItem(item);
-    
+
     item = new ElementItem(2);
     item->setPos(17 * width, 0 * height);
     addItem(item);
-    
+
     item = new ElementItem(3);
     item->setPos( 0 * width, 1 * height);
     addItem(item);
-    
+
     item = new ElementItem(4);
     item->setPos( 1 * width, 1 * height);
     addItem(item);
@@ -194,55 +207,55 @@ namespace Avogadro {
     item = new ElementItem(6);
     item->setPos(13 * width, 1 * height);
     addItem(item);
-    
+
     item = new ElementItem(7);
     item->setPos(14 * width, 1 * height);
     addItem(item);
-    
+
     item = new ElementItem(8);
     item->setPos(15 * width, 1 * height);
     addItem(item);
-    
+
     item = new ElementItem(9);
     item->setPos(16 * width, 1 * height);
     addItem(item);
-    
+
     item = new ElementItem(10);
     item->setPos(17 * width, 1 * height);
     addItem(item);
-    
+
     item = new ElementItem(11);
     item->setPos( 0 * width, 2 * height);
     addItem(item);
-    
+
     item = new ElementItem(12);
     item->setPos( 1 * width, 2 * height);
     addItem(item);
-    
+
     item = new ElementItem(13);
     item->setPos(12 * width, 2 * height);
     addItem(item);
-    
+
     item = new ElementItem(14);
     item->setPos(13 * width, 2 * height);
     addItem(item);
-    
+
     item = new ElementItem(15);
     item->setPos(14 * width, 2 * height);
     addItem(item);
-    
+
     item = new ElementItem(16);
     item->setPos(15 * width, 2 * height);
     addItem(item);
-    
+
     item = new ElementItem(17);
     item->setPos(16 * width, 2 * height);
     addItem(item);
-    
+
     item = new ElementItem(18);
     item->setPos(17 * width, 2 * height);
     addItem(item);
-    
+
     int element = 19;
     for (int i = 3; i < 5; i++) {
       for (int j = 0; j < 18; j++) {
@@ -251,33 +264,33 @@ namespace Avogadro {
         addItem(item);
       }
     }
-    
+
     item = new ElementItem(element++);
     item->setPos( 0 * width, 5 * height);
     addItem(item);
-    
+
     item = new ElementItem(element++);
     item->setPos( 1 * width, 5 * height);
     addItem(item);
-    
+
     element = 71;
-    
+
     for (int i = 2; i < 18; i++) {
       item = new ElementItem(element++);
       item->setPos(i * width, 5 * height);
       addItem(item);
     }
-    
+
     item = new ElementItem(element++);
     item->setPos( 0 * width, 6 * height);
     addItem(item);
-    
+
     item = new ElementItem(element++);
     item->setPos( 1 * width, 6 * height);
     addItem(item);
-    
+
     element = 103;
-    
+
     for (int i = 2; i < 16; i++) {
       item = new ElementItem(element++);
       item->setPos(i * width, 6 * height);
@@ -325,12 +338,12 @@ namespace Avogadro {
   {
     // Use a small title bar (Qt::Tool) with no minimise or maximise buttons
     setWindowFlags(Qt::Dialog | Qt::Tool);
-    
+
     PeriodicTableScene *table = new PeriodicTableScene;
     table->setSceneRect(-20, -20, 480, 270);
     table->setItemIndexMethod(QGraphicsScene::NoIndex);
     table->setBackgroundBrush(Qt::white);
-    
+
     setScene(table);
     setRenderHint(QPainter::Antialiasing);
     setWindowTitle("Periodic Table");
@@ -342,16 +355,16 @@ namespace Avogadro {
 
   PeriodicTableGV::PeriodicTableGV(QGraphicsScene *scene, QWidget *parent) :
     QGraphicsView(scene, parent)
-  {    
+  {
     connect(scene, SIGNAL(elementChanged(int)),
             this, SLOT(elementClicked(int)));
   }
-  
+
   void PeriodicTableGV::setSelectedElement(int)
   {
-    
+
   }
-  
+
   void PeriodicTableGV::elementClicked(int id)
   {
     emit(elementChanged(id));
--- libavogadro/src/tools/periodictablegv.h
+++ libavogadro/src/tools/periodictablegv.h
@@ -1,21 +1,21 @@
 /**********************************************************************
  PeriodicTableGV - Periodic Table Graphics View for Avogadro
- 
+
  Copyright (C) 2007 by Marcus D. Hanwell
- 
+
  This file is part of the Avogadro molecular editor project.
  For more information, see <http://avogadro.sourceforge.net/>
- 
+
  Avogadro is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation; either version 2 of the License, or
  (at your option) any later version.
- 
+
  Avogadro is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
- 
+
  You should have received a copy of the GNU General Public License
  along with this program; if not, write to the Free Software
  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
@@ -102,23 +102,23 @@ namespace Avogadro {
   class ElementDetail : public QObject, public QGraphicsItem
   {
     Q_OBJECT
-    
+
   public:
     /**
      * Constructor. Should be called with the element number for this item.
      */
     ElementDetail(int elementNumber = 0);
-    
+
     /**
      * @return the bounding rectangle of the element item.
      */
     QRectF boundingRect() const;
-    
+
     /**
      * @return the painter path which is also a rectangle in this case.
      */
     QPainterPath shape() const;
-    
+
     /**
      * This is where most of the action takes place. The element box is drawn
      * along with its symbol, proton number, mass and full name.
@@ -168,15 +168,15 @@ namespace Avogadro {
   public:
     PeriodicTableGV(QWidget *parent = 0);
     PeriodicTableGV(QGraphicsScene *scene, QWidget *parent = 0);
-    
+
     void setSelectedElement(int element);
-    
+
   private Q_SLOTS:
     void elementClicked(int element);
-    
+
   Q_SIGNALS:
     void elementChanged(int element);
-    
+
   private:
     int m_element;
   };
