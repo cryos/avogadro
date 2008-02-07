a22d8fa22f75f1ea0492a3f5e58ba1d74a566902
Marcus D. Hanwell <marcus@cryos.org>
2008-02-07 22:25:42 +0000

    Tweaked formatting to fit with ours, removed some application specific elements and tried to get it working in the Avogadro framework. More work still required as there are several application specific elements remaining before this class is self contained and able to generate isofurfaces.
    git-svn-id: https://avogadro.svn.sourceforge.net/svnroot/avogadro/trunk@1028 af78ad9f-9711-0410-ba57-a8eff4914ec4

--- libavogadro/src/engines/iso.h
+++ libavogadro/src/engines/iso.h
@@ -1,35 +1,61 @@
 /****************************************************************************
-**
-** Copyright (C) 1991-2007 Heinz van Saanen
-**
-** This file is part of the the function viewer Zhu3D.
-**
-** This file may be used under the terms of the GNU General Public
-** License version 2 as published by the Free Software Foundation
-** and appearing in the file LICENSE.GPL included in the packaging of
-** this file.
-**
-** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-**
-****************************************************************************/
-
+ **
+ ** Copyright (C) 1991-2007 Heinz van Saanen
+ **
+ ** This file is part of the the function viewer Zhu3D.
+ **
+ ** This file may be used under the terms of the GNU General Public
+ ** License version 2 as published by the Free Software Foundation
+ ** and appearing in the file LICENSE.GPL included in the packaging of
+ ** this file.
+ **
+ ** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ ** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ **
+ ****************************************************************************/
 
 /****************************************************************************
-**
-** Most of the code for the Marching Cubes and Tetrahedras and the tables are
-** from an exemplary implementation of Cory Gene Bloyd. In a first step I
-** made just minor adoptions like reformatting, interfacing or optimizations
-** in the code. Later on I made it reentrant and thread-safe with some more
-** adaptions.
-**
-** Thanks to Cory Gene Bloyd for publishing this excellent and understandable
-** piece of software. For a description of the original algorithm look at:
-**
-** http://astronomy.swin.edu.au/pbourke/modelling/polygonise/
-**
-****************************************************************************/
+ **
+ ** Most of the code for the Marching Cubes and Tetrahedras and the tables are
+ ** from an exemplary implementation of Cory Gene Bloyd. In a first step I
+ ** made just minor adoptions like reformatting, interfacing or optimizations
+ ** in the code. Later on I made it reentrant and thread-safe with some more
+ ** adaptions.
+ **
+ ** Thanks to Cory Gene Bloyd for publishing this excellent and understandable
+ ** piece of software. For a description of the original algorithm look at:
+ **
+ ** http://astronomy.swin.edu.au/pbourke/modelling/polygonise/
+ **
+ ****************************************************************************/
 
+/**********************************************************************
+ Iso - Iso class for displaying surfaces using OpenGL
+
+ Copyright (C) 2008      Marcus D. Hanwell
+ 
+ The original copyright headers are shown above. This source was originally
+ part of the Zhu3D project. Subsequent changes were made to adapt the source
+ in order to be used in the Avogadro molecular editor project.
+
+ This file is part of the Avogadro molecular editor project.
+ For more information, see <http://avogadro.sourceforge.net/>
+
+ Avogadro is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ Avogadro is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ 02110-1301, USA.
+ **********************************************************************/
 
 #ifndef ISO_H
 #define ISO_H
@@ -37,78 +63,75 @@
 #include <QList>
 #include <QThread>
 #include <cmath>
-#include <GL/gl.h>
-#include "property.h"
-#include "pinterface.h"
-#include "debug.h"
+#include <avogadro/glwidget.h>
 
+namespace Avogadro
+{
 
-// Vector structure
-struct GLvector {
-	float fX;
-	float fY;
-	float fZ;
-} __attribute__((aligned(16)));
+  // Triangle structure
+  struct triangle
+  {
+    Eigen::Vector3f p0;
+    Eigen::Vector3f p1;
+    Eigen::Vector3f p2;
+  };
 
+  // Does isosurface tessellation. Called from gldraw only
+  class IsoGen : public QThread
+  {
 
-// Triangle structure
-struct triangle {
-	GLvector p0;
-	GLvector p1;
-	GLvector p2;
-};
+  public:
 
+    // General stuff
+    IsoGen(QObject *parent = 0) : QThread(parent) { ; }
 
-// Does isosurface tessellation. Called from gldraw only
-class ISOGEN : public QThread {
+    ~IsoGen() { ; }
 
-public:
+    // Vertex/normal-lists
+    QList<triangle> normList;
+    QList<triangle> vertList;
 
-	// General stuff
-	ISOGEN(QObject *parent = 0) : QThread(parent) {};
-	~ISOGEN() {};
+    // Central functions
+    void vMarching(const long pfrom, const long pto, const long n);
 
-	// Vertex/normal-lists
-	QList<triangle> normList;
-	QList<triangle> vertList;
+  protected:
+    void run();
 
-	// Central functions
-	void vMarching(const long pfrom, const long pto, const long n);
+  private:
+    float fStepSize; // Grid density == 2.0f/sta.tgrids;
+    long totTri; // Triangles calculated in total; currently not used
+    long from, to; // Grid-distribution for multithreading
 
-protected:
-	void run();
+    // Constants/tables
+    static const float fTargetValue;
+    static const float a2fVertexOffset[8][3];
+    static const long a2iEdgeConnection[12][2];
+    static const float a2fEdgeDirection[12][3];
+    static const long a2iTetrahedronEdgeConnection[6][2];
+    static const long a2iTetrahedronsInACube[6][4];
+    static const long aiTetrahedronEdgeFlags[16];
+    static const long a2iTetrahedronTriangles[16][7];
+    static const long aiCubeEdgeFlags[256];
+    static const long a2iTriangleConnectionTable[256][16];
 
-private:
-	float fStepSize;		// Grid density == 2.0f/sta.tgrids;
-	long totTri;			// Triangles calculated in total; currently not used
-	long from, to;			// Grid-distribution for multithreading
+    // Functions
+    void vNormalizeVector(Eigen::Vector3f &rfVectorResult,
+        const Eigen::Vector3f &rfVectorSource);
+    void vGetNormal(Eigen::Vector3f &rfNormal, const float fX, const float fY,
+        const float fZ);
+    void vMarchCube1(const float fX, const float fY, const float fZ);
+    void vMarchCube2(const float fX, const float fY, const float fZ);
+    void vMarchTetrahedron(Eigen::Vector3f *pasTetrahedronPosition,
+        const float *pafTetrahedronValue);
+    void (IsoGen::*tessellation)(const float fX, const float fY, const float fZ);
 
-	// Constants/tables
-	static const float fTargetValue;
-	static const float a2fVertexOffset[8][3];
-	static const long  a2iEdgeConnection[12][2];
-	static const float a2fEdgeDirection[12][3];
-	static const long  a2iTetrahedronEdgeConnection[6][2];
-	static const long  a2iTetrahedronsInACube[6][4];
-	static const long  aiTetrahedronEdgeFlags[16];
-	static const long  a2iTetrahedronTriangles[16][7];
-	static const long  aiCubeEdgeFlags[256];
-	static const long  a2iTriangleConnectionTable[256][16];
+  // Multithreading stuff:
+      // Using own class-instance of standard iso-parsers and own calc-function
+      // Copies of Warp-instances are not reentrant/threadsafe by itself
+//      FunctionParser isoInstance;
+      float isoPar(const float xpar, const float ypar, const float zpar);
+    };
 
-	// Functions
-	void vNormalizeVector(GLvector &rfVectorResult, const GLvector &rfVectorSource);
-	void vGetNormal(GLvector &rfNormal, const float fX, const float fY, const float fZ);
-	void vMarchCube1(const float fX, const float fY, const float fZ);
-	void vMarchCube2(const float fX, const float fY, const float fZ);
-	void vMarchTetrahedron(GLvector *pasTetrahedronPosition, const float *pafTetrahedronValue);
-	void (ISOGEN::*tessellation)(const float fX, const float fY, const float fZ);
-
-	// Multithreading stuff:
-	// Using own class-instance of standard iso-parsers and own calc-function
-	// Copies of Warp-instances are not reentrant/threadsafe by itself
-	FunctionParser isoInstance;
-	float isoPar(const float xpar, const float ypar, const float zpar);
-};
-
-// ISO_H
+  }
+  // ISO_H
 #endif
