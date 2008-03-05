ad672a488f3b436b49e138093b137570ab1e5e25
Marcus D. Hanwell <marcus@cryos.org>
2008-03-05 17:04:29 +0000

    Just draw GL_LINES.
    git-svn-id: https://avogadro.svn.sourceforge.net/svnroot/avogadro/trunk@1155 af78ad9f-9711-0410-ba57-a8eff4914ec4

--- libavogadro/src/engines/orbitalengine.cpp
+++ libavogadro/src/engines/orbitalengine.cpp
@@ -219,9 +219,9 @@ namespace Avogadro {
     qDebug() << "Number of triangles = " << m_isoGen->numTriangles();
 
     // Use the GL_LINE mode to render
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-    glBegin(GL_TRIANGLES);
+    glBegin(GL_LINES);
     m_posColor.applyAsMaterials();
     for(int i=0; i < m_isoGen->numTriangles(); ++i)
     {
