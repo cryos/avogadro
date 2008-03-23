c609e28faceb5659100f1fdabb652994581c2ae5
Donald Ephraim Curtis <dcurtis3@sourceforge.net>
2008-03-23 04:07:47 +0000

    Added a set of extension template files.
    git-svn-id: https://avogadro.svn.sourceforge.net/svnroot/avogadro/trunk@1220 af78ad9f-9711-0410-ba57-a8eff4914ec4

---
+++ libavogadro/src/extensions/templateextension.cpp
@@ -0,0 +1,107 @@
+/**********************************************************************
+  Template - Extension Template
+
+  Copyright (C) 2008 by Author
+
+  This file is part of the Avogadro molecular editor project.
+  For more information, see <http://avogadro.sourceforge.net/>
+
+  Some code is based on Open Babel
+  For more information, see <http://openbabel.sourceforge.net/>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation version 2 of the License.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+ ***********************************************************************/
+
+#include "templateextension.h"
+
+using namespace std;
+using namespace OpenBabel;
+
+namespace Avogadro
+{
+
+  // this is a trick to identify what action we are taking
+  enum TemplateExtensionIndex {
+    FirstAction = 0,
+    SecondAction,
+  };
+
+  TemplateExtension::TemplateExtension( QObject *parent ) : Extension( parent )  
+  {
+    // create an action for our second action
+    QAction *action = new QAction( this );
+    action->setText( tr("First Action" ));
+    m_actions.append( action );
+    action->setData( FirstAction );
+
+    // create an action for our second action
+    action = new QAction( this );
+    action->setText( tr("Second Action" ));
+    m_actions.append( action );
+    action->setData( SecondAction );
+
+  }
+
+  TemplateExtension::~TemplateExtension()
+  {
+  }
+
+  QList<QAction *> TemplateExtension::actions() const
+  {
+    return m_actions;
+  }
+
+  // allows us to set the intended menu path for each action
+  QString TemplateExtension::menuPath(QAction *) const
+  {
+    int i = action->data().toInt();
+
+    switch ( i ) {
+      case FirstAction:
+        return tr("&Extensions") + ">" + tr("&Template");
+        break;
+      case SecondAction:
+        return tr("&Edit") + ">" + tr("&Template");
+        break;
+    }
+  }
+
+  QDockWidget * TemplateExtension::dockWidget()
+  {
+    // if we need a dock widget we can set one here
+  }
+
+  void TemplateExtension::setMolecule(Molecule *molecule)
+  {
+    m_molecule = molecule;
+  }
+
+  QUndoCommand* TemplateExtension::performAction( QAction *action, GLWidget *widget )
+  {
+
+    int i = action->data().toInt();
+
+    switch ( i ) {
+      case FirstAction:
+        // perform first action
+        break;
+      case SecondAction:
+        // perform second action
+        break;
+    }
+
+    return 0;
+  }
+
+}
+
+#include "templateextension.moc"
+
+Q_EXPORT_PLUGIN2( templateextension, Avogadro::TemplateExtensionFactory );
---
+++ libavogadro/src/extensions/templateextension.h
@@ -0,0 +1,71 @@
+/**********************************************************************
+  Template - Extension Template
+
+  Copyright (C) 2008 by Author
+
+  This file is part of the Avogadro molecular editor project.
+  For more information, see <http://avogadro.sourceforge.net/>
+
+  Some code is based on Open Babel
+  For more information, see <http://openbabel.sourceforge.net/>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation version 2 of the License.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+ ***********************************************************************/
+
+#ifndef __TEMPLATEEXTENSION_H
+#define __TEMPLATEEXTENSION_H
+
+#include <avogadro/primitive.h>
+#include <avogadro/glwidget.h>
+#include <avogadro/molecule.h>
+
+namespace Avogadro {
+
+  class TemplateExtension : public Extension
+  {
+    Q_OBJECT
+
+    public:
+      //! Constructor
+      TemplateExtension(QObject *parent=0);
+      //! Deconstructor
+      virtual ~TemplateExtension();
+
+      virtual QString name() const { return QObject::tr("Template"); }
+      virtual QString description() const { return QObject::tr("Template for developing extensions"); };
+
+      virtual QList<QAction *> actions() const;
+      virtual QString menuPath(QAction *action) const;
+
+      virtual QDockWidget * dockWidget();
+      virtual QUndoCommand* performAction(QAction *action, GLWidget *widget);
+
+      virtual setMolecule(Molecule *molecule);
+
+    private:
+      QList<QAction *> m_actions;
+      Molecule *m_molecule;
+
+    private Q_SLOTS:
+
+  };
+
+  class TemplateExtensionFactory : public QObject, public ExtensionFactory
+  {
+    Q_OBJECT
+    Q_INTERFACES(Avogadro::ExtensionFactory);
+
+    public:
+      Extension *createInstance(QObject *parent = 0) { return new TemplateExtension(parent); }
+  };
+
+} // end namespace Avogadro
+
+#endif
