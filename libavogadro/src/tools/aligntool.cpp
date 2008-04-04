430ccf54490976fcaaff9d78bd54f8b51afddc1f
Marcus D. Hanwell <marcus@cryos.org>
2008-04-04 19:40:32 +0000

    Added new align tool. Currently it just aligns the selected atom to the origin, if there are two selected atoms they will be aligned along the chosen axis.
    git-svn-id: https://avogadro.svn.sourceforge.net/svnroot/avogadro/trunk@1229 af78ad9f-9711-0410-ba57-a8eff4914ec4

---
+++ libavogadro/src/tools/aligntool.cpp
@@ -0,0 +1,304 @@
+/**********************************************************************
+  AlignTool - AlignTool Tool for Avogadro
+
+  Copyright (C) 2008 Marcus D. Hanwell
+
+  This file is part of the Avogadro molecular editor project.
+  For more information, see <http://avogadro.sourceforge.net/>
+
+  Avogadro is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  Avogadro is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+  02110-1301, USA.
+ **********************************************************************/
+
+#include "aligntool.h"
+#include "navigate.h"
+
+#include <avogadro/primitive.h>
+#include <avogadro/color.h>
+#include <avogadro/glwidget.h>
+
+#include <openbabel/obiter.h>
+#include <openbabel/generic.h>
+
+#include <cmath>
+
+#include <QDebug>
+#include <QtPlugin>
+#include <QLabel>
+#include <QPushButton>
+#include <QComboBox>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+
+using namespace std;
+using namespace OpenBabel;
+using namespace Eigen;
+
+namespace Avogadro {
+
+  AlignTool::AlignTool(QObject *parent) : Tool(parent),  m_numSelectedAtoms(0),
+  m_axis(2), m_settingsWidget(0)
+  {
+    QAction *action = activateAction();
+    action->setIcon(QIcon(QString::fromUtf8(":/measure/measure.png")));
+    action->setToolTip(tr("Align Molecules\n\n"
+          "Left Mouse: \tSelect up to two atoms.\n"
+          "\tThe first atom is centered at the origin.\n"
+          "\tThe second atom is aligned to the selected axis.\n"
+          "Right Mouse: \tReset alignment."));
+    action->setShortcut(Qt::Key_F12);
+
+    // clear the selected atoms
+    int size = m_selectedAtoms.size();
+    for(int i=0; i<size; i++)
+    {
+      m_selectedAtoms[i] = NULL;
+    }
+  }
+
+  AlignTool::~AlignTool()
+  {
+  }
+
+  QUndoCommand* AlignTool::mousePress(GLWidget *widget, const QMouseEvent *event)
+  {
+    Molecule *molecule = widget->molecule();
+    if(!molecule)
+      return 0;
+
+    //! List of hits from initial click
+    QList<GLHit> m_hits = widget->hits(event->pos().x()-2, event->pos().y()-2, 5, 5);
+
+    // If there's a left button (and no modifier keys) continue adding to the list
+    if(m_hits.size() && (event->buttons() & Qt::LeftButton && event->modifiers() == Qt::NoModifier))
+    {
+      if(m_hits[0].type() != Primitive::AtomType)
+        return 0;
+
+      Atom *atom = (Atom *)molecule->GetAtom(m_hits[0].name());
+
+      if(m_numSelectedAtoms < 2)
+      {
+        // Select another atom
+        m_selectedAtoms[m_numSelectedAtoms++] = atom;
+        widget->update();
+        if (m_numSelectedAtoms)
+        {
+          m_neighborList.clear();
+          // We really want the "connected fragment" since a Molecule can contain
+          // multiple user visible molecule fragments
+          OBMolAtomDFSIter iter(molecule, atom->GetIdx());
+          Atom *tmpNeighbor;
+          do
+          {
+            tmpNeighbor = static_cast<Atom*>(&*iter);
+            m_neighborList.append(tmpNeighbor);
+          } while ((iter++).next()); // this returns false when we've gone looped through the fragment
+        }
+      }
+    }
+    // Right button or Left Button + modifier (e.g., Mac)
+    else
+    {
+      int size = m_selectedAtoms.size();
+      for(int i=0; i<size; i++)
+      {
+        m_selectedAtoms[i] = NULL;
+      }
+      m_numSelectedAtoms = 0;
+      widget->update();
+    }
+    return 0;
+  }
+
+  QUndoCommand* AlignTool::mouseMove(GLWidget*, const QMouseEvent *)
+  {
+    return 0;
+  }
+
+  QUndoCommand* AlignTool::mouseRelease(GLWidget*, const QMouseEvent*)
+  {
+    return 0;
+  }
+
+  QUndoCommand* AlignTool::wheel(GLWidget*widget, const QWheelEvent*event)
+  {
+    // let's set the reference to be the center of the visible
+    // part of the molecule.
+    Eigen::Vector3d atomsBarycenter(0., 0., 0.);
+    double sumOfWeights = 0.;
+    std::vector<OpenBabel::OBNodeBase*>::iterator i;
+    for ( Atom *atom = static_cast<Atom*>(widget->molecule()->BeginAtom(i));
+          atom; atom = static_cast<Atom*>(widget->molecule()->NextAtom(i))) {
+      Eigen::Vector3d transformedAtomPos = widget->camera()->modelview() * atom->pos();
+      double atomDistance = transformedAtomPos.norm();
+      double dot = transformedAtomPos.z() / atomDistance;
+      double weight = exp(-30. * (1. + dot));
+      sumOfWeights += weight;
+      atomsBarycenter += weight * atom->pos();
+    }
+    atomsBarycenter /= sumOfWeights;
+
+    Navigate::zoom(widget, atomsBarycenter, - MOUSE_WHEEL_SPEED * event->delta());
+    widget->update();
+
+    return NULL;
+  }
+
+  bool AlignTool::paint(GLWidget *widget)
+  {
+    if(0 < m_numSelectedAtoms)
+    {
+      glColor3f(1.0,0.0,0.0);
+      widget->painter()->setColor(1.0, 0.0, 0.0);
+      Vector3d pos = m_selectedAtoms[0]->pos();
+      double radius = 0.18 + etab.GetVdwRad(m_selectedAtoms[0]->GetAtomicNum()) * 0.3;
+
+      Vector3d xAxis = widget->camera()->backTransformedXAxis();
+      Vector3d zAxis = widget->camera()->backTransformedZAxis();
+
+      // relative position of the text on the atom
+      Vector3d textRelPos = radius * (zAxis + xAxis);
+
+      Vector3d textPos = pos+textRelPos;
+      widget->painter()->drawText(textPos, "*1");
+      double renderRadius = widget->radius(m_selectedAtoms[0]) + 0.05;
+      widget->painter()->drawSphere(pos, renderRadius);
+
+      if(m_numSelectedAtoms >= 2)
+      {
+        glColor3f(0.0,1.0,0.0);
+        widget->painter()->setColor(0.0, 1.0, 0.0);
+        pos = m_selectedAtoms[1]->pos();
+        renderRadius = widget->radius(m_selectedAtoms[1]) + 0.05;
+        widget->painter()->drawSphere(pos, renderRadius);
+        Vector3d textPos = pos+textRelPos;
+        radius = 0.18 + etab.GetVdwRad(m_selectedAtoms[1]->GetAtomicNum()) * 0.3;
+        widget->painter()->drawText(textPos, "*2");
+      }
+    }
+
+    return true;
+  }
+
+  void AlignTool::align()
+  {
+    // Align the molecule along the selected axis
+    if (m_numSelectedAtoms >= 1)
+    {
+      // Translate the first selected atom to the origin
+      MatrixP3d atomTranslation;
+      atomTranslation.loadTranslation(-m_selectedAtoms[0]->pos());
+      foreach(Primitive *p, m_neighborList)
+      {
+        Atom *a = static_cast<Atom *>(p);
+        a->setPos(atomTranslation * a->pos());
+        a->update();
+      }
+    }
+    if (m_numSelectedAtoms >= 2)
+    {
+      // Now line up the line from atom[0] to atom[1] with the axis selected
+      double alpha, beta, gamma;
+      alpha = beta = gamma = 0.0;
+
+      Vector3d pos = m_selectedAtoms[1]->pos();
+      pos.normalize();
+      Vector3d axis;
+
+      if (m_axis == 0) // x-axis
+        axis = Vector3d(1., 0., 0.);
+      else if (m_axis == 1) // y-axis
+        axis = Vector3d(0., 1., 0.);
+      else if (m_axis == 2) // z-axis
+        axis = Vector3d(0., 0., 1.);
+
+      // Calculate the angle of the atom from the axis
+      double angle = acos(axis.dot(pos));
+
+      // Get the axis for the rotation
+      axis = axis.cross(pos);
+      axis.normalize();
+
+      // Now to load up the rotation matrix and rotate the molecule
+      MatrixP3d atomRotation;
+      atomRotation.loadRotation3(-angle, axis);
+
+      // Now to rotate the fragment
+      foreach(Primitive *p, m_neighborList)
+      {
+        Atom *a = static_cast<Atom *>(p);
+        a->setPos(atomRotation * a->pos());
+        a->update();
+      }
+    }
+  }
+
+  QWidget* AlignTool::settingsWidget()
+  {
+    if(!m_settingsWidget) {
+      m_settingsWidget = new QWidget;
+
+      QLabel *labelAxis = new QLabel(tr("Axis:"));
+      labelAxis->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+      labelAxis->setMaximumHeight(15);
+
+      // Small popup with 10 most common elements for organic chemistry
+      // (and extra for "other" to bring up periodic table window)
+      QComboBox *comboAxis = new QComboBox(m_settingsWidget);
+      comboAxis->addItem("x");
+      comboAxis->addItem("y");
+      comboAxis->addItem("z");
+      comboAxis->setCurrentIndex(2);
+
+      QPushButton *buttonAlign = new QPushButton(m_settingsWidget);
+      buttonAlign->setText(tr("Align"));
+      connect(buttonAlign, SIGNAL(clicked()), this, SLOT(align()));
+
+      QHBoxLayout *hLayout = new QHBoxLayout();
+      hLayout->addWidget(labelAxis);
+      hLayout->addWidget(comboAxis);
+      QVBoxLayout *layout = new QVBoxLayout();
+      layout->addLayout(hLayout);
+      layout->addWidget(buttonAlign);
+      layout->addStretch(1);
+      m_settingsWidget->setLayout(layout);
+
+      connect(comboAxis, SIGNAL(currentIndexChanged(int)),
+              this, SLOT(axisChanged(int)));
+
+      connect(m_settingsWidget, SIGNAL(destroyed()),
+              this, SLOT(settingsWidgetDestroyed()));
+    }
+
+    return m_settingsWidget;
+  }
+
+  void AlignTool::axisChanged(int axis)
+  {
+    // Axis to use - x=0, y=1, z=2
+    m_axis = axis;
+  }
+
+  void AlignTool::settingsWidgetDestroyed()
+  {
+    m_settingsWidget = 0;
+  }
+
+}
+
+#include "aligntool.moc"
+
+Q_EXPORT_PLUGIN2(aligntool, Avogadro::AlignToolFactory)
---
+++ libavogadro/src/tools/aligntool.h
@@ -0,0 +1,100 @@
+/**********************************************************************
+  AlignTool - AlignTool Tool for Avogadro
+
+  Copyright (C) 2008 Marcus D. Hanwell
+
+  This file is part of the Avogadro molecular editor project.
+  For more information, see <http://avogadro.sourceforge.net/>
+
+  Avogadro is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  Avogadro is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+  02110-1301, USA.
+ **********************************************************************/
+
+#ifndef __ALIGNTOOL_H
+#define __ALIGNTOOL_H
+
+#include <avogadro/glwidget.h>
+#include <avogadro/tool.h>
+
+#include <QGLWidget>
+#include <QObject>
+#include <QStringList>
+#include <QVarLengthArray>
+#include <QImage>
+#include <QAction>
+
+namespace Avogadro {
+
+ class AlignTool : public Tool
+  {
+    Q_OBJECT
+
+    public:
+      //! Constructor
+      AlignTool(QObject *parent = 0);
+      //! Destructor
+      virtual ~AlignTool();
+
+      //! \name Description methods
+      //@{
+      //! Tool Name (i.e. Align)
+      virtual QString name() const { return(tr("Align")); }
+      //! Tool Description (i.e. Align molecules)
+      virtual QString description() const { return(tr("Align molecules")); }
+      //@}
+
+      //! \name Tool Methods
+      //@{
+      //! \brief Callback methods for ui.actions on the canvas.
+      /*!
+        */
+      virtual QUndoCommand* mousePress(GLWidget *widget, const QMouseEvent *event);
+      virtual QUndoCommand* mouseRelease(GLWidget *widget, const QMouseEvent *event);
+      virtual QUndoCommand* mouseMove(GLWidget *widget, const QMouseEvent *event);
+      virtual QUndoCommand* wheel(GLWidget *widget, const QWheelEvent *event);
+
+      virtual bool paint(GLWidget *widget);
+
+      virtual QWidget *settingsWidget();
+
+    public Q_SLOTS:
+      void axisChanged(int axis);
+      void align();
+
+    private:
+      QVarLengthArray<Atom *, 2> m_selectedAtoms;
+      int m_numSelectedAtoms;
+      int m_axis;
+      QList<Primitive *> m_neighborList;
+
+      QWidget *m_settingsWidget;
+
+    private Q_SLOTS:
+      void settingsWidgetDestroyed();
+
+  };
+
+  class AlignToolFactory : public QObject, public ToolFactory
+    {
+      Q_OBJECT
+      Q_INTERFACES(Avogadro::ToolFactory)
+
+      public:
+        Tool *createInstance(QObject *parent = 0) { return new AlignTool(parent); }
+    };
+
+} // end namespace Avogadro
+
+#endif
