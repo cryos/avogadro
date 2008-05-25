d5040bcc14d0834b718900bff2405c831e876569
Benoit Jacob <jacob.benoit.1@gmail.com>
2008-05-25 21:40:51 +0000

    patch from Simon: improve and make use of TestIfWeNeedFPermissive.cmake This solves issues with gcc >= 4.3.1 being stricter than current boost code. In fact the present test is slightly stricter in that 4.3.0 also needs -fpermissive. GCC 4.2 doesn't require it.
    git-svn-id: https://avogadro.svn.sourceforge.net/svnroot/avogadro/trunk@1436 af78ad9f-9711-0410-ba57-a8eff4914ec4

--- cmake/modules/TestIfWeNeedFPermissive.cmake
+++ cmake/modules/TestIfWeNeedFPermissive.cmake
@@ -1,7 +1,7 @@
 include (CheckCXXSourceCompiles)
 
 macro (TEST_IF_WE_NEED_FPERMISSIVE)
-	SET(_TEST_IF_WE_NEED_FPERMISSIVE_SOURCE_CODE
+  SET(_TEST_IF_WE_NEED_FPERMISSIVE_SOURCE_CODE
 #This checks if we need -fpermissive to work around a problem with Boost/Python and GCC >= GCC 4.3.1
 #See: http://gcc.gnu.org/gcc-4.3/porting_to.html
 #GCC by default no longer accepts code such as 
@@ -12,11 +12,18 @@ struct counted_ptr
 {
   auto_ptr<_Tp> auto_ptr();
 };
+int main() {return 0;}
 "
-	)
+#
+#
+#
+  )
 	
-	CHECK_CXX_SOURCE_COMPILES("${_TEST_IF_WE_NEED_FPERMISSIVE_SOURCE_CODE}" ${FPERMISSIVE_NO})
-ENDMACRO (TEST_IF_WE_NEED_FPERMISSIVE)
+  CHECK_CXX_SOURCE_COMPILES("${_TEST_IF_WE_NEED_FPERMISSIVE_SOURCE_CODE}" COMPILES_WITHOUT_FPERMISSIVE)
+
+  set (NEED_FPERMISSIVE NOT COMPILES_WITHOUT_FPERMISSIVE)
+
+endmacro (TEST_IF_WE_NEED_FPERMISSIVE)
 #but will issue the diagnostic
 #
 #error: declaration of 'auto_ptr<_Tp> counted_ptr<_Tp>::auto_ptr()'
