e738a324333f619c11616d86c622a1e8be8b099a
Geoffrey Hutchison <geoff@geoffhutchison.net>
2008-05-28 18:49:11 +0000

    Found another Mac SelectAll cursor fix.
    git-svn-id: https://avogadro.svn.sourceforge.net/svnroot/avogadro/trunk@1451 af78ad9f-9711-0410-ba57-a8eff4914ec4

--- libavogadro/src/tools/manipulatetool.cpp
+++ libavogadro/src/tools/manipulatetool.cpp
@@ -173,7 +173,12 @@ namespace Avogadro {
     {
       m_leftButtonPressed = true;
       // Set the cursor - this needs to be reset to Qt::ArrowCursor after
+      // Currently, there's a Qt/Mac bug -- SizeAllCursor looks like a spreadsheet cursor
+#ifdef Q_WS_MAC
+      widget->setCursor(Qt::CrossCursor);
+#else 
       widget->setCursor(Qt::SizeAllCursor);
+#endif
     }
 
     // On a Mac, click and hold the Shift key
