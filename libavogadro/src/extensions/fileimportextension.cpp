99405de5e3afa66f55c633628f2921ce87f12d01
Marcus D. Hanwell <marcus@cryos.org>
2008-11-26 00:27:57 +0000

    Added a new file import dialog, initially concentrating purely on xyz files and amber parameter files. Still pretty rough around the edges.
    git-svn-id: https://avogadro.svn.sourceforge.net/svnroot/avogadro/trunk@1648 af78ad9f-9711-0410-ba57-a8eff4914ec4

---
+++ libavogadro/src/extensions/fileimportextension.cpp
@@ -0,0 +1,220 @@
+/**********************************************************************
+  FileImportExtension - Extension for generating POV-Ray rendered images
+
+  Copyright (C) 2008 Marcus D. Hanwell
+
+  This file is part of the Avogadro molecular editor project.
+  For more information, see <http://avogadro.sourceforge.net/>
+
+  Avogadro is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  Avogadro is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+  02110-1301, USA.
+ **********************************************************************/
+
+#include "fileimportextension.h"
+
+#include <avogadro/molecule.h>
+#include <avogadro/bond.h>
+#include <avogadro/toolgroup.h>
+
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QFileDialog>
+#include <QCoreApplication>
+#include <QProcess>
+#include <QFileInfo>
+#include <QTextStream>
+#include <QFile>
+#include <QDebug>
+
+#include <openbabel/mol.h>
+#include <openbabel/obconversion.h>
+
+
+
+using namespace OpenBabel;
+using namespace std;
+
+namespace Avogadro
+{
+
+  FileImportExtension::FileImportExtension(QObject* parent) : Extension(parent),
+    m_glwidget(0), m_molecule(0), m_fileImportDialog(0)
+  {
+    QAction* action = new QAction(this);
+    action->setText(tr("Import file..."));
+    m_actions.append(action);
+  }
+
+  FileImportExtension::~FileImportExtension()
+  {
+    if (m_fileImportDialog) {
+      delete m_fileImportDialog;
+      m_fileImportDialog = 0;
+    }
+  }
+
+  QList<QAction *> FileImportExtension::actions() const
+  {
+    return m_actions;
+  }
+
+  QString FileImportExtension::menuPath(QAction*) const
+  {
+    return tr("&File");
+  }
+
+  QUndoCommand* FileImportExtension::performAction(QAction *, GLWidget *widget)
+  {
+    m_glwidget = widget;
+    if (!m_fileImportDialog) {
+      m_fileImportDialog = new FileImportDialog();
+      connect(m_fileImportDialog->selectFileName, SIGNAL(clicked()),
+              this, SLOT(selectFileName()));
+      connect(m_fileImportDialog->selectFileName2, SIGNAL(clicked()),
+              this, SLOT(selectFileName2()));
+      connect(m_fileImportDialog->buttonBox, SIGNAL(accepted()),
+              this, SLOT(import()));
+      connect(m_fileImportDialog->buttonBox, SIGNAL(rejected()),
+              m_fileImportDialog, SLOT(hide()));
+      m_fileImportDialog->show();
+    }
+    else {
+      m_fileImportDialog->show();
+    }
+    return 0;
+  }
+
+  void FileImportExtension::writeSettings(QSettings &settings) const
+  {
+    Extension::writeSettings(settings);
+  }
+
+  void FileImportExtension::readSettings(QSettings &settings)
+  {
+    Extension::readSettings(settings);
+  }
+
+  void FileImportExtension::setMolecule(Molecule *molecule)
+  {
+    m_molecule = molecule;
+  }
+
+  void FileImportExtension::readParmFile(Molecule *mol)
+  {
+    // Now the task of reading the parm file. Right now just reading in bonds
+    QFile file(m_fileImportDialog->fileName2->text());
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)){
+      qDebug() << "Unable to read file" << m_fileImportDialog->fileName2->text();
+      return;
+    }
+    QTextStream in(&file);
+
+    while (!in.atEnd()) {
+      processLine(&in, mol);
+    }
+  }
+
+  void FileImportExtension::processLine(QTextStream *in, Molecule *mol)
+  {
+    // First truncate the line, remove trailing white space and check
+    QString line = in->readLine();
+    QString key = line;
+    key = key.trimmed();
+//    qDebug() << "Line" << line;
+
+    if (key == "%FLAG BONDS_WITHOUT_HYDROGEN") {
+      qDebug() << "Reading in bonds...";
+      line = in->readLine(); // Throw away this line
+      line = "";
+
+      QStringList list;
+      while (line[0] != '%') {
+        line = in->readLine();
+//        qDebug() << line;
+        list += line.split(" ", QString::SkipEmptyParts);
+        if (list.size() == 30) {
+          for (int i = 0; i <= 27; i += 3) {
+            Bond *bond = mol->newBond();
+            bond->setAtoms(list.at(i).toInt()/3, list.at(i+1).toInt()/3);
+            //bond->setOrder(list.at(i+2).toInt());
+          }
+          list.clear();
+        }
+      }
+    }
+
+  }
+
+  void FileImportExtension::import()
+  {
+    QString fileName = m_fileImportDialog->fileName->text();
+    QApplication::setOverrideCursor(Qt::WaitCursor);
+    OBConversion conv;
+    OBFormat     *inFormat = conv.FormatFromExt(( fileName.toAscii() ).data() );
+    if ( !inFormat || !conv.SetInFormat( inFormat ) ) {
+      QApplication::restoreOverrideCursor();
+      QMessageBox::warning(m_fileImportDialog, tr("Avogadro"),
+          tr("Cannot read file format of file %1.").arg(fileName));
+      return;
+    }
+
+    ifstream ifs;
+    ifs.open((fileName.toAscii()).data());
+    if (!ifs) { // shouldn't happen, already checked file above
+      QApplication::restoreOverrideCursor();
+      QMessageBox::warning(m_fileImportDialog, tr("Avogadro"),
+          tr("Cannot read file %1.").arg( fileName ) );
+      return;
+    }
+
+    OBMol *obmol = new OBMol;
+    // Turn off bond perception
+    conv.AddOption("b", OBConversion::INOPTIONS);
+    if (conv.Read(obmol, &ifs)) {
+      Molecule *mol = new Molecule;
+      mol->setOBMol(obmol);
+      mol->setFileName(fileName);
+      Molecule *oldMol = m_molecule;
+      qDebug() << "Attempting to read parm file...";
+      readParmFile(mol);
+      qDebug() << "Parm file read...";
+      m_glwidget->setMolecule(mol);
+      oldMol->deleteLater();
+    }
+
+    QApplication::restoreOverrideCursor();
+  }
+
+  void FileImportExtension::selectFileName()
+  {
+    QString fileName = QFileDialog::getOpenFileName(m_fileImportDialog,
+      tr("Open chemical file format"), m_fileImportDialog->fileName->text(),
+      tr("Chemical files (*.xyz)"));
+    m_fileImportDialog->fileName->setText(fileName);
+  }
+
+  void FileImportExtension::selectFileName2()
+  {
+    QString fileName = QFileDialog::getOpenFileName(m_fileImportDialog,
+      tr("Open parameter file"), m_fileImportDialog->fileName->text(),
+      tr("Chemical files (*.parm7)"));
+    m_fileImportDialog->fileName2->setText(fileName);
+  }
+
+} // End namespace Avogadro
+
+#include "fileimportextension.moc"
+
+Q_EXPORT_PLUGIN2(fileimportextension, Avogadro::FileImportExtensionFactory)
---
+++ libavogadro/src/extensions/fileimportextension.h
@@ -0,0 +1,126 @@
+/**********************************************************************
+  FileImportExtension - Extension for importing files
+
+  Copyright (C) 2008 Marcus D. Hanwell
+
+  This file is part of the Avogadro molecular editor project.
+  For more information, see <http://avogadro.sourceforge.net/>
+
+  Avogadro is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  Avogadro is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+  02110-1301, USA.
+ **********************************************************************/
+
+#ifndef FILEIMPORTEXTENSION_H
+#define FILEIMPORTEXTENSION_H
+
+#include "ui_fileimportdialog.h"
+
+#include <avogadro/glwidget.h>
+#include <avogadro/extension.h>
+
+class QTextStream;
+
+namespace Avogadro
+{
+
+  class FileImportDialog;
+
+  class FileImportExtension : public Extension
+  {
+  Q_OBJECT
+
+  public:
+    FileImportExtension(QObject* parent = 0);
+    virtual ~FileImportExtension();
+
+    virtual QString name() const { return QObject::tr("File import"); }
+    virtual QString description() const
+    {
+      return QObject::tr("Import chemical files");
+    }
+
+    /**
+     * @return a list of actions which this widget can perform
+     */
+    virtual QList<QAction *> actions() const;
+
+    /**
+     * @return the menu path for the specified action
+     */
+    virtual QString menuPath(QAction* action) const;
+
+    /**
+     * @param action The action that triggered the calls.
+     * @param widget The currently active GLWidget (feedback to the user).
+     * @return An undo command for this action.
+     */
+    virtual QUndoCommand* performAction(QAction *action, GLWidget *widget);
+
+    /**
+     * Save the settings for this extension.
+     * @param settings Settings variable to write settings to.
+     */
+    virtual void writeSettings(QSettings &settings) const;
+
+    /**
+     * Read the settings for this extension.
+     * @param settings Settings variable to read settings from.
+     */
+    virtual void readSettings(QSettings &settings);
+
+  public Q_SLOTS:
+
+    /**
+     * Slot to change the current molecule.
+     */
+    void setMolecule(Molecule *molecule);
+
+  private:
+    GLWidget* m_glwidget;
+    QList<QAction *> m_actions;
+    Molecule *m_molecule;
+    FileImportDialog *m_fileImportDialog;
+
+    void readParmFile(Molecule *mol);
+    void processLine(QTextStream *in, Molecule *mol);
+
+  private Q_SLOTS:
+    void import();
+    void selectFileName();
+    void selectFileName2();
+
+  };
+
+  class FileImportDialog : public QDialog, public Ui::FileImportDialog
+  {
+    public:
+      FileImportDialog(QDialog *parent=0) : QDialog(parent) {
+        setupUi(this);
+      }
+  };
+
+  class FileImportExtensionFactory : public QObject, public PluginFactory
+  {
+    Q_OBJECT
+    Q_INTERFACES(Avogadro::PluginFactory)
+    AVOGADRO_EXTENSION_FACTORY(FileImportExtension,
+        tr("File Import Extension"),
+        tr("Extension for importing chemical file format files."))
+
+  };
+
+} // End namespace Avogadro
+
+#endif
