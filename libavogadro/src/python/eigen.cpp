78e63e3dc6d0716bd9ca440db1ddd207c1e6b77e
Tim Vandermeersch <tim.vandermeersch@gmail.com>
2008-12-03 03:38:13 +0000

    Use numpy, Add Vector3f, Vector3i, Transform3d.
    Altough it now works with numpy, I get lots of warnings from numpy/arrayobject.h...
    
    
    git-svn-id: https://avogadro.svn.sourceforge.net/svnroot/avogadro/trunk@1686 af78ad9f-9711-0410-ba57-a8eff4914ec4

--- libavogadro/src/python/eigen.cpp
+++ libavogadro/src/python/eigen.cpp
@@ -1,48 +1,152 @@
 #include <Python.h>
-#include <Numeric/arrayobject.h> 
+#include <numpy/arrayobject.h> 
 #include <boost/python.hpp>
-#include <boost/python/numeric.hpp>
 #include <boost/python/tuple.hpp>
 
-#include <Eigen/Core>
+#include <Eigen/Geometry>
+
+#include <iostream>
 
 using namespace boost::python;
 
-  struct Vector3d_to_python_array
+  /***********************************************************************
+   *
+   * Vector3x = Vector3d, Vector3f, Vector3i
+   *
+   ***********************************************************************/
+  
+  template <class Vector3x>
+  struct Vector3x_to_python_array
   {
-    static PyObject* convert(Eigen::Vector3d const &vec)
+    struct innerclass
     {
-      return incref(numeric::array(make_tuple(vec.x(), vec.y(), vec.z())).ptr());
-    }
-    
-    static PyObject* convert(Eigen::Vector3d *vec)
+      //
+      //  Eigen::Vector3x --> python array
+      //
+      static PyObject* convert(Vector3x const &vec)
+      {
+        int dims[1] = { 3 };
+        PyObject *result = PyArray_FromDims(1, dims, PyArray_DOUBLE);
+        
+        // copy the data
+        double *data = (double*) reinterpret_cast<PyArrayObject*>(result)->data;
+        data[0] = vec.x();
+        data[1] = vec.y();
+        data[2] = vec.z();
+        
+        return incref(result);
+      }
+ 
+      //
+      //  Eigen::Vector3x * --> python array
+      //    
+      static PyObject* convert(Vector3x *vec)
+      {
+        if (!vec)
+          throw_error_already_set();
+ 
+        int dims[1] = { 3 };
+        PyObject *result = PyArray_FromDims(1, dims, PyArray_DOUBLE);
+        
+        // copy the data
+        double *data = (double*) reinterpret_cast<PyArrayObject*>(result)->data;
+        data[0] = vec->x();
+        data[1] = vec->y();
+        data[2] = vec->z();
+        
+        return incref(result);
+      }
+
+      //
+      //  const Eigen::Vector3x * --> python array
+      //
+      static PyObject* convert(const Vector3x *vec)
+      {
+        if (!vec)
+          throw_error_already_set();
+
+        int dims[1] = { 3 };
+        PyObject *result = PyArray_FromDims(1, dims, PyArray_DOUBLE);
+        
+        // copy the data
+        double *data = (double*) reinterpret_cast<PyArrayObject*>(result)->data;
+        data[0] = vec->x();
+        data[1] = vec->y();
+        data[2] = vec->z();
+        
+        return incref(result);
+      }
+    };
+   
+    Vector3x_to_python_array()
     {
-      if (!vec)
-        throw_error_already_set();
-      
-      return incref(numeric::array(make_tuple(vec->x(), vec->y(), vec->z())).ptr());
+      to_python_converter<Vector3x, innerclass>();
+      to_python_converter<Vector3x*, innerclass>();
+      to_python_converter<const Vector3x*, innerclass>();
     }
 
-    static PyObject* convert(const Eigen::Vector3d *vec)
-    {
-      if (!vec)
-        throw_error_already_set();
-
-      return incref(numeric::array(make_tuple(vec->x(), vec->y(), vec->z())).ptr());
-    }
   };
 
-  struct Vector3d_from_python_array
+  template <class Vector3x>
+  struct Vector3x_from_python_array
   {
-    Vector3d_from_python_array()
+    typedef typename Vector3x::Scalar Scalar;
+    
+    Vector3x_from_python_array()
     {
-      converter::registry::push_back( &convertible, &construct, type_id<Eigen::Vector3d>() );
+      // Insert an rvalue from_python converter at the tail of the
+      // chain. Used for implicit conversions
+      //
+      //  python array --> Vector3x
+      //
+      // used for:
+      //
+      //  void function(Vector3x vec)
+      //  void function(Vector3x & vec)
+      //  void function(const Vector3x & vec)
+      //
+      converter::registry::push_back( &convertible, &construct, type_id<Vector3x>() );
+
+      
+      // Insert an lvalue from_python converter
+      //
+      //  python array --> Vector3x*
+      //
+      // used for:
+      //  
+      //  void function(const Vector3x * vec)
+      converter::registry::insert( &convert, type_id<Vector3x>() );
+ 
     }
 
-    static void* convertible(PyObject *obj_ptr)
+    static void* convert(PyObject *obj_ptr)
     {
       if (!PyArray_Check(obj_ptr))
         return 0;
+
+      PyArrayObject *array = reinterpret_cast<PyArrayObject*>(obj_ptr);
+
+      // check the dimensions
+      if (array->nd != 1)
+        throw_error_already_set(); // the array has at least two dimensions (matrix)
+  
+      if (array->dimensions[0] != 3)
+        throw_error_already_set(); // the 1D array does not have exactly 3 elements
+
+      //double *values = reinterpret_cast<double*>(array->data);
+      Scalar *values = reinterpret_cast<Scalar*>(array->data);
+      
+      return new Vector3x(values[0], values[1], values[2]);
+    }
+ 
+    static void* convertible(PyObject *obj_ptr)
+    {
+      std::cout << "convertible()" << std::endl;
+      if (!PyArray_Check(obj_ptr))
+        return 0;
+      
+      std::cout << "convertible ( YES )" << std::endl;
+      
       return obj_ptr;
     }
 
@@ -57,31 +161,119 @@ using namespace boost::python;
       if (array->dimensions[0] != 3)
         throw_error_already_set(); // the 1D array does not have exactly 3 elements
 
-      double *values = reinterpret_cast<double*>(array->data);
+      //double *values = reinterpret_cast<double*>(array->data);
+      Scalar *values = reinterpret_cast<Scalar*>(array->data);
     
-      void *storage = ((converter::rvalue_from_python_storage<Eigen::Vector3d>*)data)->storage.bytes;
-      new (storage) Eigen::Vector3d(values[0], values[1], values[2]);
+      void *storage = ((converter::rvalue_from_python_storage<Vector3x>*)data)->storage.bytes;
+      new (storage) Vector3x(values[0], values[1], values[2]);
       data->convertible = storage;
     }
   };
 
+  /***********************************************************************
+   *
+   * Transform3d
+   *
+   ***********************************************************************/
+  
+  struct Transform3d_to_python_array
+  {
+    struct innerclass
+    {
+      //
+      //  Eigen::Transform3d --> python array (4x4)
+      //
+      static PyObject* convert(Eigen::Transform3d const &trans)
+      {
+        return incref(numeric::array(
+              make_tuple(
+                make_tuple(trans(0,0), trans(1,0), trans(2,0), trans(3,0)),
+                make_tuple(trans(0,1), trans(1,1), trans(2,1), trans(3,1)),
+                make_tuple(trans(0,2), trans(1,2), trans(2,2), trans(3,2)),
+                make_tuple(trans(0,3), trans(1,3), trans(2,3), trans(3,3))
+              )
+            ).ptr());
+      }
+    };
+   
+    Transform3d_to_python_array()
+    {
+      to_python_converter<Eigen::Transform3d, innerclass>();
+    }
+
+  };
+
+  struct Transform3d_from_python_array
+  {
+    Transform3d_from_python_array()
+    {
+      // Insert an rvalue from_python converter at the tail of the
+      // chain. Used for implicit conversions
+      //
+      //  python array --> Eigen::Transform3d
+      //
+      // used for:
+      //
+      //  void function(Eigen::Transform3d vec)
+      //  void function(Eigen::Transform3d & vec)
+      //  void function(const Eigen::Transform3d & vec)
+      //
+      converter::registry::push_back( &convertible, &construct, type_id<Eigen::Transform3d>() );
+    }
+
+    static void* convertible(PyObject *obj_ptr)
+    {
+      if (!PyArray_Check(obj_ptr))
+        return 0;
+      
+      return obj_ptr;
+    }
+
+    static void construct(PyObject *obj_ptr, converter::rvalue_from_python_stage1_data *data)
+    {
+      PyArrayObject *array = reinterpret_cast<PyArrayObject*>(obj_ptr);
+      
+      // check the dimensions
+      if (array->nd != 2)
+        throw_error_already_set(); // the array has at least two dimensions (matrix)
+      
+      if ((array->dimensions[0] != 4) || (array->dimensions[1] != 4))
+        throw_error_already_set(); // the 1D array does not have exactly 3 elements
+
+      double *values = reinterpret_cast<double*>(array->data);
+ 
+      void *storage = ((converter::rvalue_from_python_storage<Eigen::Transform3d>*)data)->storage.bytes;
+      new (storage) Eigen::Transform3d();
+      
+      double *dataPtr = reinterpret_cast<double*>(storage);
+
+      for (int i = 0; i < 16; ++i)
+        dataPtr[i] = values[i];
+
+      data->convertible = storage;
+    }
+  };
+
+
 void export_Eigen()
 {
   import_array(); // needed for NumPy 
-  to_python_converter<Eigen::Vector3d, Vector3d_to_python_array>();
-  to_python_converter<Eigen::Vector3d*, Vector3d_to_python_array>();
-  to_python_converter<const Eigen::Vector3d*, Vector3d_to_python_array>();
-  Vector3d_from_python_array();
+  
+  // Eigen::Vector3d
+  Vector3x_to_python_array<Eigen::Vector3d>();
+  Vector3x_from_python_array<Eigen::Vector3d>();
+  // Eigen::Vector3f
+  Vector3x_to_python_array<Eigen::Vector3f>();
+  Vector3x_from_python_array<Eigen::Vector3f>();
+  // Eigen::Vector3i
+  Vector3x_to_python_array<Eigen::Vector3i>();
+  Vector3x_from_python_array<Eigen::Vector3i>();
 
-  /*
-  class_<EigenTest>("EigenTest")
-    .def("vectorPtr", &EigenTest::vectorPtr, return_value_policy<return_by_value>())
-    .def("vectorRef", &EigenTest::vectorRef, return_value_policy<return_by_value>())
-    .def("vectorConstPtr", &EigenTest::vectorConstPtr, return_value_policy<return_by_value>())
-    .def("vectorConstRef", &EigenTest::vectorConstRef, return_value_policy<return_by_value>())
-    .def("vectorConstPtrConst", &EigenTest::vectorConstPtrConst, return_value_policy<return_by_value>())
-    ;
-  */
+  // Eigen::Transform3d
+  Transform3d_to_python_array();
+  Transform3d_from_python_array();
+
+
 
 
 }
