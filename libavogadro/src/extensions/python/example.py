daa1e67379821a52d19f867c26f75790279545c6
Tim Vandermeersch <tim.vandermeersch@gmail.com>
2008-12-22 21:49:01 +0000

    Add a QUndoCommand derived class to example.py and use it...
    git-svn-id: https://avogadro.svn.sourceforge.net/svnroot/avogadro/trunk@1854 af78ad9f-9711-0410-ba57-a8eff4914ec4

--- libavogadro/src/extensions/python/example.py
+++ libavogadro/src/extensions/python/example.py
@@ -2,33 +2,57 @@ import Avogadro
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
-class Extension():
+class AddAtomCommand(QUndoCommand):
+  def __init__(self, molecule, atomicNumber):
+    super(AddAtomCommand, self).__init__(QString("Add Atom - Python"))
+    self.molecule = molecule
+    self.atomicNumber = atomicNumber
+    self.atomId = None
+
+  def redo(self):
+    atom = self.molecule.newAtom()
+    atom.atomicNumber = self.atomicNumber
+    # save the id for undo method
+    self.atomId = atom.id
+
+  def undo(self):
+    self.molecule.deleteAtom(self.atomId)
+    self.atomId = None
+
+
+class Extension(QObject):
   def __init__(self):
-    # keep a reference to all actions to make sure garbage collection doesn't 
-    # delete our actions
-    self._actions = []
+    QObject.__init__(self)
+    # keep a reference to all commands to make sure garbage collection doesn't 
+    # delete them
+    self.commands = []
 
   def actions(self):
-    action = QAction(None)
+    actions = []
+
+    action = QAction(self)
     action.setText("Add Atom")
-    self._actions.append(action)
+    actions.append(action)
     
-    action = QAction(None)
+    action = QAction(self)
     action.setText("Clear molecule")
-    self._actions.append(action)
+    actions.append(action)
     
     #action = QAction(None)
     #action.setText("Action 3")
     #self._actions.append(action)
 
-    return self._actions
+    return actions
 
   def performAction(self, action, glwidget):
     if action.text() == "Add Atom":
-      atom = glwidget.molecule.newAtom()
-      atom.atomicNumber = 6
+      # AddAtomCommand(parent, molecule, atomicNumber)
+      command = AddAtomCommand(glwidget.molecule, 6)
+      self.commands.append(command) # prevent deletion
+      return command
     elif action.text() == "Clear molecule":
       glwidget.molecule.clear()
+      return None 
 
     return None
   
