2ea3007bd42383da7830926070791278da1b4854
Marcus D. Hanwell <marcus@cryos.org>
2008-12-29 21:13:12 -0500

    Removed calls to private API that should not be exposed.

--- libavogadro/src/python/atom.cpp
+++ libavogadro/src/python/atom.cpp
@@ -13,10 +13,6 @@ void export_Atom()
 {
   // define function pointers to handle overloading
   void (Atom::*setPos_ptr)(const Eigen::Vector3d&) = &Atom::setPos;
-  void (Atom::*addBond_ptr1)(Bond*) = &Atom::addBond;
-  void (Atom::*addBond_ptr2)(unsigned long int) = &Atom::addBond;
-  void (Atom::*removeBond_ptr1)(Bond*) = &Atom::removeBond;
-  void (Atom::*removeBond_ptr2)(unsigned long int) = &Atom::removeBond;
 
   class_<Avogadro::Atom, bases<Avogadro::Primitive>, boost::noncopyable>("Atom", no_init)
     // read/write properties
@@ -28,11 +24,6 @@ void export_Atom()
     .add_property("neighbors", &Atom::neighbors)
     .add_property("valence", &Atom::valence)
     .add_property("isHydrogen", &Atom::isHydrogen)
-    // real functions
-    .def("addBond", addBond_ptr1)
-    .def("addBond", addBond_ptr2)
-    .def("removeBond", removeBond_ptr1)
-    .def("removeBond", removeBond_ptr2)
     ;
 
 }
