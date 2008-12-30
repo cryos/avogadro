08c00beac093462069139fdcbda4b477626a1e14
Tim Vandermeersch <tim.vandermeersch@gmail.com>
2008-12-30 23:51:36 +0100

    Some win32 specific changes

--- scripts/cmake-vs2005.bat
+++ scripts/cmake-vs2005.bat
@@ -1,3 +1,3 @@
 @echo off
 
-cmake.exe .. -DOPENBABEL2_INCLUDE_DIR=C:\openbabel-SVN\include -DOPENBABEL2_LIBRARIES=C:\openbabel-SVN\windows-vc2005\obdll.dll -DEIGEN2_INCLUDE_DIR=C:\eigen\include -G "Visual Studio 8 2005"
+cmake.exe .. -DOPENBABEL2_INCLUDE_DIR=C:\openbabel-trunk\include -DOPENBABEL2_LIBRARIES=C:\openbabel-trunk\output\Release\openbabel-2.lib -DEIGEN2_INCLUDE_DIR=C:\eigen2 -DBOOST_ROOT="c:\Program Files\boost\boost_1_36_0" -DNUMPY_INCLUDE_DIR="C:\Python25\Lib\site-packages\numpy\core\include" -DLIBRARY_OUPUT_PATH=c:\avogadro-git\output -DEXECUTABLE_OUPUT_PATH=c:\avogadro-git\output -DENABLE_GLSL:=ON -G "Visual Studio 9 2008"
