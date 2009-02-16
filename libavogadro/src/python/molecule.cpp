2bc9ddc2de373f7fbe01e18cc8f97e34f1409380
Geoff Hutchison <geoff@geoffhutchison.net>
2009-02-16 13:16:27 -0500

    Fix python module linking. Split MoleculeList into new compile unit. Ensure correct moc file is included (previously used any "molecule.moc"!)

--- libavogadro/src/python/molecule.cpp
+++ libavogadro/src/python/molecule.cpp
@@ -10,36 +10,6 @@
 
 #include <openbabel/mol.h>
 
-#include "molecule.h"
-
-namespace Avogadro {
-    
-  MoleculeList* MoleculeList::instance()
-  {
-    static MoleculeList *instance = 0;
-    if (!instance)
-      instance = new MoleculeList;
-    return instance;
-  }
-
-  Molecule* MoleculeList::addMolecule()
-  {
-    Molecule *mol;
-    mol = new Molecule();
-    m_molecules.append(mol);
-    connect(mol, SIGNAL(destroyed()), this, SLOT(moleculeDestroyed()));
-    return mol;
-  }
-
-  void MoleculeList::moleculeDestroyed()
-  {
-    Molecule* mol = static_cast<Molecule*>(sender());
-    int index = m_molecules.indexOf(mol);
-    m_molecules.removeAt(index);  
-  }
-
-}
-
 using namespace boost::python;
 using namespace Avogadro;
 
@@ -152,20 +122,5 @@ void export_Molecule()
     .def("translate", &Molecule::translate)
     ;
   
-  class_<MoleculeList, boost::noncopyable>("MoleculeList", no_init)
-    .add_property("instance", make_function(&MoleculeList::instance, return_value_policy<reference_existing_object>()))
-    .add_property("numMolecules", &MoleculeList::numMolecules)
-    .def("addMolecule", &MoleculeList::addMolecule, return_value_policy<reference_existing_object>())
-    .def("at", &MoleculeList::at, return_value_policy<reference_existing_object>())
-    ;
-
-  // module's DATA: Avogadro.molecules
-  MoleculeList *moleculeList = MoleculeList::instance();
-  reference_existing_object::apply<MoleculeList*>::type converter;
-  PyObject* pyobj = converter( moleculeList );
-  object real_obj = object( handle<>( pyobj ) );
-  scope().attr("molecules") = real_obj;
-
 }
 
-#include "molecule.moc"
