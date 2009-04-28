3fca9550e45b458a62b1663aa61ee0fcbeffa21b
Marcus D. Hanwell <marcus@cryos.org>
2009-04-28 01:00:06 -0400

    Added a few more tests for the Molecule class.

--- libavogadro/tests/moleculetest.cpp
+++ libavogadro/tests/moleculetest.cpp
@@ -1,7 +1,7 @@
 /**********************************************************************
   Molecule - MoleculeTest class provides unit testing for the Molecule class
 
-  Copyright (C) 2008 Marcus D. Hanwell
+  Copyright (C) 2008-2009 Marcus D. Hanwell
 
   This file is part of the Avogadro molecular editor project.
   For more information, see <http://avogadro.openmolecules.net/>
@@ -39,75 +39,123 @@ class MoleculeTest : public QObject
 {
   Q_OBJECT
 
-  private:
-    Molecule *m_molecule; /// Molecule object for use by the test class.
+private:
+  Molecule *m_molecule; /// Molecule object for use by the test class.
 
-  private slots:
-    /**
-     * Called before the first test function is executed.
-     */
-    void initTestCase();
+  /**
+   * Prepare a starting molecule of known geometry for other tests.
+   */
+  void prepareMolecule();
 
-    /**
-     * Called after the last test function is executed.
-     */
-    void cleanupTestCase();
+private slots:
+  /**
+   * Called before the first test function is executed.
+   */
+  void initTestCase();
 
-    /**
-     * Called before each test function is executed.
-     */
-    void init();
+  /**
+   * Called after the last test function is executed.
+   */
+  void cleanupTestCase();
 
-    /**
-     * Called after every test function.
-     */
-    void cleanup();
+  /**
+   * Called before each test function is executed.
+   */
+  void init();
 
-    /**
-     * Tests the addition of new Atom objects to the Molecule.
-     */
-    void addAtom();
+  /**
+   * Called after every test function.
+   */
+  void cleanup();
 
-    /**
-     * Tests the setting of the Atom position.
-     */
-    void setAtomPos();
+  /**
+   * Tests the addition of new Atom objects to the Molecule.
+   */
+  void addAtom();
 
-    /**
-     * Tests the retrieval of the Atom position.
-     */
-    void atomPos();
+  /**
+   * Tests the setting of the Atom position.
+   */
+  void setAtomPos();
 
-    /**
-     * Tests the access of Atom objects by index.
-     */
-    void atom();
+  /**
+   * Tests the retrieval of the Atom position.
+   */
+  void atomPos();
 
-    /**
-     * Tests the access of Atom objects by index.
-     */
-    void atomById();
+  /**
+   * Tests the access of Atom objects by index.
+   */
+  void atom();
 
-    /**
-     * Tests the access of all Atom objects in Molecule.
-     */
-    void atoms();
+  /**
+   * Tests the access of Atom objects by index.
+   */
+  void atomById();
 
-    /**
-     * Tests the deletion of Atom objects from the Molecule.
-     */
-    void removeAtom();
+  /**
+   * Tests the access of all Atom objects in Molecule.
+   */
+  void atoms();
 
-    /**
-     * Tests the addition of new Bond objects to the Molecule.
-     */
-    void addBond();
+  /**
+   * Tests the deletion of Atom objects from the Molecule.
+   */
+  void removeAtom();
 
-    /**
-     * Tests the deletion of Bond objects from the Molecule.
-     */
-    void removeBond();
+  /**
+   * Tests the addition of new Bond objects to the Molecule.
+   */
+  void addBond();
+
+  /**
+   * Tests the deletion of Bond objects from the Molecule.
+   */
+  void removeBond();
+
+  /**
+   * Tests the calculation of the center of the Molecule.
+   */
+  void center();
+
+  /**
+   * Tests the calculation of the normal vector of the Molecule.
+   */
+  void normalVector();
+
+  /**
+   * Tests the calculation of the radius of the Molecule.
+   */
+  void radius();
+
+  /**
+   * Tests the determination of the farthest atom.
+   */
+  void farthestAtom();
+
+  /**
+   * Tests the translation of the Molecule.
+   */
+  void translate();
 };
+
+void MoleculeTest::prepareMolecule()
+{
+  Atom *a1 = m_molecule->addAtom();
+  a1->setPos(Vector3d(0.0, 0.0, 0.0));
+  Atom *a2 = m_molecule->addAtom();
+  a2->setPos(Vector3d(1.5, 0.0, 0.0));
+  Atom *a3 = m_molecule->addAtom();
+  a3->setPos(Vector3d(0.0, 1.5, 0.0));
+  Atom *a4 = m_molecule->addAtom();
+  a4->setPos(Vector3d(0.0, 0.0, 1.5));
+  Bond *b1 = m_molecule->addBond();
+  b1->setAtoms(a1->id(), a2->id(), 1);
+  Bond *b2 = m_molecule->addBond();
+  b2->setAtoms(a2->id(), a3->id(), 1);
+  Bond *b3 = m_molecule->addBond();
+  b3->setAtoms(a3->id(), a4->id(), 1);
+}
 
 void MoleculeTest::initTestCase()
 {
@@ -203,6 +251,46 @@ void MoleculeTest::removeBond()
   QVERIFY(m_molecule->numBonds() == 1);
   m_molecule->removeBond(0ul);
   QVERIFY(m_molecule->numBonds() == 0);
+}
+
+void MoleculeTest::center()
+{
+  prepareMolecule();
+  Vector3d center = m_molecule->center();
+  QCOMPARE(center.x(), 0.375);
+  QCOMPARE(center.y(), 1.5 / 4.0);
+  QCOMPARE(center.z(), 1.5 / 4.0);
+}
+
+void MoleculeTest::normalVector()
+{
+  Vector3d normal = m_molecule->normalVector();
+  QCOMPARE(normal.x(), -0.57735026919);
+  QCOMPARE(normal.y(), -0.57735026919);
+  QCOMPARE(normal.z(), -0.57735026919);
+}
+
+void MoleculeTest::radius()
+{
+  QCOMPARE(m_molecule->radius(), 1.24373429638327494);
+}
+
+void MoleculeTest::farthestAtom()
+{
+  QVERIFY(m_molecule->farthestAtom()->index() == 1);
+}
+
+void MoleculeTest::translate()
+{
+  m_molecule->translate(Vector3d(1.0, 1.1, 1.2));
+  QCOMPARE(m_molecule->atom(0)->pos()->x(), 1.0);
+  QCOMPARE(m_molecule->atom(0)->pos()->y(), 1.1);
+  QCOMPARE(m_molecule->atom(0)->pos()->z(), 1.2);
+  QCOMPARE(m_molecule->atom(1)->pos()->x(), 2.5);
+  // Check the center was correctly updated
+  QCOMPARE(m_molecule->center().x(), 1.5 / 4.0 + 1.0);
+  QCOMPARE(m_molecule->center().y(), 1.5 / 4.0 + 1.1);
+  QCOMPARE(m_molecule->center().z(), 1.5 / 4.0 + 1.2);
 }
 
 QTEST_MAIN(MoleculeTest)
