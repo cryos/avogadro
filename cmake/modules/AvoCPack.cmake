5a7b73c7ab506b6f4ef8394137105aec51cde1e0
Tim Vandermeersch <tim.vandermeersch@gmail.com>
2009-05-01 00:40:36 +0200

    cmake/modules/AvoCPack.cmake: Changes to correctly include all python stuff
    cmake/modules/FindNumpy.cmake: Small bug fix, wasen't working here
    
    libavogadro/src/python/CMakeLists.txt: Set library SUFFIX property to ".pyd" on WIN32
    
    scripts/cmake-vs2005.bat: update for python & GLSL support

--- cmake/modules/AvoCPack.cmake
+++ cmake/modules/AvoCPack.cmake
@@ -12,36 +12,42 @@ set (CPACK_CREATE_DESKTOP_LINKS "avogadro")
 
 if (WIN32)
   # Set the directories to defaults if not set
-  if(NOT openbabel_DIR)
-    set (openbabel_DIR "C:/src/openbabel-2-2-x/output/Release")
-  endif(NOT openbabel_DIR)
-  if(NOT openbabel_SRCDIR)
-    set (openbabel_SRCDIR "C:/src/openbabel-2-2-x")
-  endif(NOT openbabel_SRCDIR)
-  if(NOT qt_BINDIR)
-    set (qt_BINDIR "C:/qt/4.4.3/bin")
-  endif(NOT qt_BINDIR)
-  if(NOT python_DIR)
-    set (python_DIR "C:/src/python-2.6.1") # already contains sip & numpy modules (see wiki)
-  endif(NOT python_DIR)
-  if(NOT boost_python_DIR)
-    set (boost_python_DIR "C:/src/boost_1_38_0")
-  endif(NOT boost_python_DIR)
-  if(NOT pyqt_DIR)
-    set (pyqt_DIR "C:/src/PyQt-win-gpl-4.4.4")
-  endif(NOT pyqt_DIR)
+ 
+  ##############################################
+  # Zlib                                       #
+  ##############################################
+  find_file(zlib_DLL "zlib1.dll" PATHS
+      "C:/src/zlib-1.2.3/projects/visualc6/Win32_DLL_Release"
+      ${zlib_DIR}
+  ) 
+  install(FILES ${zlib_DLL} DESTINATION bin)
 
-  # OpenBabel
-  set(openbabel_DEPS
-    "${openbabel_SRCDIR}/windows-vc2005/zlib1.dll"
-    "${openbabel_SRCDIR}/windows-vc2005/libxml2.dll"
-    "${openbabel_SRCDIR}/windows-vc2005/iconv.dll"
-    "${openbabel_SRCDIR}/windows-vc2005/libinchi.dll")
-  install(FILES ${openbabel_DEPS} DESTINATION bin)
+  ##############################################
+  # libxml2                                    #
+  ##############################################
+  find_path(libxml2_DIR "libxml-2.0.pc.in" PATHS
+      "C:/src/libxml2"
+      "C:/src/libxml2-2.7.3"
+      "C:/src/libxml2-2-7-3"
+  ) 
+  find_file(libxml2_DLL "libxml2.dll" PATHS
+      "${libxml2_DIR}/win32/bin.msvc"
+      "${libxml2_DIR}/bin"
+      "${libxml2_DIR}/lib"
+  ) 
+  install(FILES ${libxml2_DLL} DESTINATION bin)
 
-  file(GLOB openbabel_FILES "${openbabel_DIR}/*.obf")
-  set(openbabel_FILES ${openbabel_FILES} "${openbabel_DIR}/openbabel-2.dll")
-  install(FILES ${openbabel_FILES} DESTINATION bin)
+  ##############################################
+  # OpenBabel                                  #
+  ##############################################
+  find_path(openbabel_SRCDIR "openbabel-2.0.pc.in" PATHS
+      "C:/src/openbabel"
+      "C:/src/openbabel-2-2-1"
+      "C:/src/openbabel-2-2-x"
+      "C:/src/openbabel-2.2.1"
+      "C:/src/openbabel-2.2.x"
+  )
+
   # Data files needed by OpenBabel
   file(GLOB openbabel_FILES "${openbabel_SRCDIR}/data/*.txt")
   install(FILES ${openbabel_FILES} DESTINATION bin)
@@ -52,35 +58,106 @@ if (WIN32)
   file(GLOB openbabel_FILES "${openbabel_SRCDIR}/data/*.ff")
   install(FILES ${openbabel_FILES} DESTINATION bin)
 
-  # Qt
+  find_path(openbabel_BINDIR "openbabel-2.dll" PATHS
+      "${openbabel_SRCDIR}/output/Release"
+      "${openbabel_SRCDIR}/build/src/Release"
+      "${openbabel_SRCDIR}/src/Release"
+      "${openbabel_SRCDIR}/Release"
+      "${openbabel_SRCDIR}"
+  )
+  set(openbabel_DLLs 
+      "${openbabel_BINDIR}/openbabel-2.dll"
+      "${openbabel_BINDIR}/inchi.dll")
+  install(FILES ${openbabel_DLLs} DESTINATION bin)
+
+  file(GLOB openbabel_FORMATS "${openbabel_BINDIR}/*.obf")
+  install(FILES ${openbabel_FORMATS} DESTINATION bin)
+ 
+  ##############################################
+  # Qt                                         #
+  ##############################################
+  find_path(qt_BINDIR "QtCore4.dll" PATHS
+      "C:/src/qt-4.4.3/bin"
+      "C:/src/qt-4.4.4/bin"
+      "C:/src/qt-4.4.5/bin"
+      "C:/src/qt-4.5.0/bin"
+      "C:/src/qt-4.5.1/bin"
+  )
   set(qt_DEPS
     "${qt_BINDIR}/QtCore4.dll"
     "${qt_BINDIR}/QtGui4.dll"
     "${qt_BINDIR}/QtOpenGL4.dll")
   install(FILES ${qt_DEPS} DESTINATION bin)
 
+  ##############################################
+  # GLSL shaders (Optional)                    #
+  ##############################################  
+  if(ENABLE_GLSL AND GLEW_FOUND)
+    find_file(glew_DLL "glew32.dll" PATHS
+        "C:/src/glew/bin"
+    )
+    install(FILES ${glew_DLL} DESTINATION bin)
+  endif(ENABLE_GLSL AND GLEW_FOUND)
+
+  ##############################################
+  # Python (Optional)                          #
+  ##############################################
   if(ENABLE_PYTHON AND ALL_PYTHON_FOUND)
     # Python support - optionally enabled and installed
-
+    
+    #
     # python library
-    set(python_DEPS "${python_DIR}/libs/python26.dll")
-    install(FILES ${python_DEPS} DESTINATION bin)
+    #
+    find_path(python_DIR "pyconfig.h.in" PATHS
+        "C:/src/Python-2.6.2"
+        "C:/src/Python-2.6.1"
+    )
+    find_file(python_DLL "python26.dll" PATHS
+        "${python_DIR}/Libs"
+        "${python_DIR}/DLLs"
+        "${python_DIR}/bin"
+        "${python_DIR}/PCbuild"
+    )
+    install(FILES ${python_DLL} DESTINATION bin)
+
+    #
+    # boost python
+    #
+    find_path(boost_DIR "boost.png" PATHS
+        "C:/src/boost_1_38_0"
+        "C:/src/boost_1_37_0"
+        "C:/src/boost_1_36_0"
+    )
+    find_file(boost_python_DLL "boost_python-vc90-mt-1_38.dll" PATHS
+        "${boost_DIR}/lib/"
+        "${boost_DIR}/bin/"
+    )
+    install(FILES ${boost_python_DLL} DESTINATION bin)
 
     # lib/*: (includes all sip & numpy runtime files needed)
     file(GLOB python_lib_FILES "${python_DIR}/lib/*.py")
     install(FILES ${python_lib_FILES} DESTINATION bin/lib)
-    #file(GLOB_RECURSE python_lib_FILES "${python_DIR}/lib/site-packages/*.*")
-    #install(FILES ${python_lib_FILES} DESTINATION bin/lib)
-
-    add_custom_command(TARGET python_lib_FILES POST_BUILD
-      COMMAND ${CMAKE_COMMAND} -E copy "${python_DIR}/lib" bin)
-
-    # boost python
-    set(boost_python_DEPS
-      "${boost_python_DIR}/lib/boost_python-vc90-mt-1_38.dll")
-    install(FILES ${boost_python_DEPS} DESTINATION bin)
-
+    # lib/encodings/*.py
+    file(GLOB python_lib_encodings_FILES "${python_DIR}/lib/encodings/*.py")
+    install(FILES ${python_lib_encodings_FILES} DESTINATION bin/lib/encodings)
+    #
+    # sip 
+    #
+    set(python_lib_sip_FILES
+        "${python_DIR}/lib/site-packages/sip.pyd"
+        "${python_DIR}/lib/site-packages/sipconfig.py"
+    )
+    install(FILES ${python_lib_sip_FILES} DESTINATION bin/lib/site-packages)
+    #    
+    # numpy
+    #
+    install(DIRECTORY ${python_DIR}/lib/site-packages/numpy DESTINATION bin/lib/site-packages)
+    # 
     # PyQt4
+    #
+    find_path(pyqt_DIR "pyqtconfig.py.in" PATHS
+        "C:/src/PyQt-win-gpl-4.4.4"
+    )
     set(pyqt_DEPS
       "${pyqt_DIR}/__init__.py"
       "${pyqt_DIR}/Qt/Qt.pyd"
@@ -89,6 +166,20 @@ if (WIN32)
       "${pyqt_DIR}/QtOpenGL/QtOpenGL.pyd"
       "${pyqt_DIR}/QtCore/QtCore.pyd")
     install(FILES ${pyqt_DEPS} DESTINATION bin/lib/site-packages/PyQt4)
+    # 
+    # Avogadro python module
+    #
+    install(FILES ${Avogadro_BINARY_DIR}/lib/Avogadro.pyd DESTINATION bin/lib/site-packages)
+    #
+    # Avogadro plugin scripts
+    #
+    file(GLOB toolScripts "${Avogadro_SOURCE_DIR}/libavogadro/src/tools/python/*.py")
+    install(FILES ${toolScripts} DESTINATION bin/toolScripts)
+    file(GLOB engineScripts "${Avogadro_SOURCE_DIR}/libavogadro/src/engines/python/*.py")
+    install(FILES ${engineScripts} DESTINATION bin/engineScripts)
+    file(GLOB extensionScripts "${Avogadro_SOURCE_DIR}/libavogadro/src/extensions/python/*.py")
+    install(FILES ${extensionScripts} DESTINATION bin/extensionScripts)
+
   endif(ENABLE_PYTHON AND ALL_PYTHON_FOUND)
 
 endif (WIN32)
--- scripts/cmake-vs2005.bat
+++ scripts/cmake-vs2005.bat
@@ -1,3 +1,34 @@
 @echo off
 
-cmake.exe .. -DOPENBABEL2_INCLUDE_DIR=C:\openbabel-trunk\include -DOPENBABEL2_LIBRARIES=C:\openbabel-trunk\output\Release\openbabel-2.lib -DEIGEN2_INCLUDE_DIR=C:\eigen2 -DBOOST_ROOT="c:\Program Files\boost\boost_1_36_0" -DNUMPY_INCLUDE_DIR="C:\Python25\Lib\site-packages\numpy\core\include" -DLIBRARY_OUPUT_PATH=c:\avogadro-git\output -DEXECUTABLE_OUPUT_PATH=c:\avogadro-git\output -DENABLE_GLSL:=ON -G "Visual Studio 9 2008"
+SET build_system="NMake Makefiles"
+SET build_type="Release"
+
+REM General
+
+SET PATH=%PATH%;C:\src\qt-4.5.1\bin
+SET openbabel_dir=C:\src\openbabel-2.2.1
+SET openbabel_lib=%openbabel_dir%\output\Release\openbabel-2.lib
+SET eigen2_dir=C:\src\eigen-2.0.1
+
+REM GLSL Support
+
+SET glew_dir=C:\src\glew\include
+SET glew_lib=C:\src\glew\lib\glew32.lib
+
+REM Python Support
+
+SET python_dir=C:\src\Python-2.6.2
+SET python_include=%python_dir%\Include
+SET python_lib=%python_dir%\libs\python26.lib
+SET python_exe=%python_dir%\libs\python.exe
+SET boost_dir=C:\src\boost_1_38_0
+
+REM Run cmake with GLSL & Python support
+cmake.exe .. -DOPENBABEL2_INCLUDE_DIR=%openbabel_dir%\include -DOPENBABEL2_LIBRARIES=%openbabel_lib% -DEIGEN2_INCLUDE_DIR=%eigen2_dir% -DGLEW_INCLUDE_DIR=%glew_dir% -DGLEW_LIBRARY=%glew_lib% -DENABLE_GLSL:=ON -DPYTHON_INCLUDE_PATH=%python_include% -DPYTHON_LIBRARY=%python_lib% -DPYTHON_EXECUTABLE=%python_exe% -DBOOST_ROOT=%boost_dir% -DCMAKE_BUILD_TYPE=%build_type% -G %build_system% 
+
+
+REM Run cmake with GLSL (no python)
+REM cmake.exe .. -DOPENBABEL2_INCLUDE_DIR=%openbabel_dir%\include -DOPENBABEL2_LIBRARIES=%openbabel_lib% -DEIGEN2_INCLUDE_DIR=%eigen2_dir% -DGLEW_INCLUDE_DIR=%glew_dir% -DGLEW_LIBRARY=%glew_lib% -DENABLE_GLSL:=ON -DCMAKE_BUILD_TYPE=%build_type% -G %build_system% 
+
+REM Run cmake without GLSL & python support
+REM cmake.exe .. -DOPENBABEL2_INCLUDE_DIR=%openbabel_dir%\include -DOPENBABEL2_LIBRARIES=%openbabel_lib% -DEIGEN2_INCLUDE_DIR=%eigen2_dir% -DCMAKE_BUILD_TYPE=%build_type% -G %build_system% 
