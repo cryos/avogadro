f3eb0641c184b809714f9ac09c2cd92bdc768851
Geoff Hutchison <geoff@geoffhutchison.net>
2009-05-13 20:02:54 -0400

    Add fine-grained control of the maybeSave() message box.

--- avogadro/src/mainwindow.cpp
+++ avogadro/src/mainwindow.cpp
@@ -1355,12 +1355,28 @@ namespace Avogadro
   bool MainWindow::maybeSave()
   {
     if ( isWindowModified() ) {
-      QMessageBox::StandardButton ret;
-      ret = QMessageBox::warning( this, tr( "Avogadro" ),
-          tr( "The document has been modified.\n"
-            "Do you want to save your changes?" ),
-          QMessageBox::Save | QMessageBox::Discard
-          | QMessageBox::Cancel );
+      // We're using the property interface to QMessageBox, rather than
+      // the static functions. This is more work, but gives us some nice
+      // fine-grain control. This helps both on Windows and Mac 
+      // look more "native."
+      QMessageBox msgBox(QMessageBox::Warning, 
+                         tr( "Avogadro" ),
+                         tr( "Do you want to save the changes you made in the document?" ),
+                         QMessageBox::Save | QMessageBox::Discard
+                         | QMessageBox::Cancel,
+                         this);
+
+      // On Mac, this will make a sheet relative to the window
+      msgBox.setWindowModality(Qt::WindowModal);
+      // second line of text
+      msgBox.setInformativeText(tr("Your changes will be lost if you don't save them." ));
+      msgBox.setDefaultButton(QMessageBox::Save);
+
+      // OK, now add shortcuts for save and discard
+      msgBox.button(QMessageBox::Save)->setShortcut(QKeySequence(tr("Ctrl+S", "Save")));
+      msgBox.button(QMessageBox::Discard)->setShortcut(QKeySequence(tr("Ctrl+D", "Discard")));
+
+      int ret = msgBox.exec();
 
       if ( ret == QMessageBox::Save )
         return save();
