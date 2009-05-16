836f1b88eaca4a99b241bd09ce9e81d94fb4f526
Tim Vandermeersch <tim.vandermeersch@gmail.com>
2009-05-16 14:06:53 +0200

    Some improvements to the Animation class. Remove duplicated code from the animation extension and use Animation class. Also added optional "dynamic bonds" for animating QM reactions. TODO: add a plot widget to display the energy

--- libavogadro/src/animation.h
+++ libavogadro/src/animation.h
@@ -37,6 +37,7 @@ namespace Avogadro {
 
   class Molecule;
 
+  class AnimationPrivate;
   class A_EXPORT Animation : public QObject
   {
     Q_OBJECT
@@ -47,18 +48,76 @@ namespace Avogadro {
       //! Deconstructor
       virtual ~Animation();
     
-      void setFrames(std::vector< std::vector< Eigen::Vector3d> *> frames);
+      /**
+       * Set the molecule to animate.
+       */
       void setMolecule(Molecule *molecule);
+      /**
+       * Set the frames for the animation. By default, the conformers in the 
+       * molecule are used as animation frames. However, for trajectory files
+       * that don't contain any topology, it is needed to read in the the 
+       * molecule topology before the trajectory. The trajectory frames can 
+       * be used to call setFrames() later.
+       */
+      void setFrames(std::vector< std::vector< Eigen::Vector3d> *> frames);
 
-   public Q_SLOTS:
-      void setDuration(int i);
+      /**
+       * @return The number of frames per second.
+       */
+      int fps() const;
+      /**
+       * @return The loopCount (0 = repeat forever).
+       */
+      int loopCount() const;
+      /**
+       * @return The total number of frames in the animation.
+       */
+      int numFrames() const;
+      /**
+       * @return True if dynamic bond detection is enabled.
+       */
+      bool dynamicBonds() const;
+
+    Q_SIGNALS:
+      /**
+       * This signal is emitted when the current frame is changed (i.e. setFrame() called)
+       */
+      void frameChanged(int);
+
+    public Q_SLOTS:
+      /**
+       * Set the number of frames per second.
+       */
+      void setFps(int fps);
+      /**
+       * Set the loop count. (0 = repeat forever)
+       */
       void setLoopCount(int loops);
+      /**
+       * Set the current frame. 
+       */
       void setFrame(int i);
+      /**
+       * Enable/disable dynamic bond detection. For QM reactions for example.
+       */
+      void setDynamicBonds(bool enable);
 
+      /**
+       * Start the animation (at current frame).
+       */
       void start();
+      /**
+       * Pause the animation.
+       */
+      void pause();
+      /**
+       * Stop the animation (and return to first frame).
+       */
       void stop();
 
     private:
+      AnimationPrivate * const d;
+      
       Molecule *m_molecule;
       QTimeLine *m_timeLine;
       std::vector< std::vector< Eigen::Vector3d> *> m_originalConformers;
