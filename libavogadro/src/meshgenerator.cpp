cryos/avogadro

commit 4678b8d53bab0e5104515276da0332fdb619dc74

Fix a variety of minor compiler errors.

libavogadro/src/meshgenerator.cpp
@@ -140,7 +140,7 @@ namespace Avogadro {
 
   inline float MeshGenerator::offset(float val1, float val2)
   {
-    if (val2 - val1 == 0.0f)
+    if (val2 - val1 < 1.0e-5f)
       return 0.5;
     return (m_iso - val1) / (val2 - val1);
   }

libavogadro/src/cube.cpp
@@ -183,9 +183,9 @@ namespace Avogadro {
   {
     int i, j, k;
     // Calculate how many steps each coordinate is along its axis
-    i = (pos.x() - m_min.x()) / m_spacing.x();
-    j = (pos.y() - m_min.y()) / m_spacing.y();
-    k = (pos.z() - m_min.z()) / m_spacing.z();
+    i = int((pos.x() - m_min.x()) / m_spacing.x());
+    j = int((pos.y() - m_min.y()) / m_spacing.y());
+    k = int((pos.z() - m_min.z()) / m_spacing.z());
     return i*m_points.y()*m_points.z() + j*m_points.z() + k;
   }
 
@@ -193,17 +193,17 @@ namespace Avogadro {
   {
     // Calculate how many steps each coordinate is along its axis
     int i, j, k;
-    i = (pos.x() - m_min.x()) / m_spacing.x();
-    j = (pos.y() - m_min.y()) / m_spacing.y();
-    k = (pos.z() - m_min.z()) / m_spacing.z();
+    i = int((pos.x() - m_min.x()) / m_spacing.x());
+    j = int((pos.y() - m_min.y()) / m_spacing.y());
+    k = int((pos.z() - m_min.z()) / m_spacing.z());
     return Vector3i(i, j, k);
   }
 
   Vector3d Cube::position(unsigned int index) const
   {
     int x, y, z;
-    x = static_cast<int>(index / (m_points.y()*m_points.z()));
-    y = static_cast<int>((index - (x*m_points.y()*m_points.z())) / m_points.z());
+    x = int(index / (m_points.y()*m_points.z()));
+    y = int((index - (x*m_points.y()*m_points.z())) / m_points.z());
     z = index % m_points.z();
     return Vector3d(x * m_spacing.x() + m_min.x(),
                     y * m_spacing.y() + m_min.y(),

libavogadro/src/engines/hbondengine.cpp
@@ -146,7 +146,7 @@ namespace Avogadro {
 
   void HBondEngine::setWidth(int value)
   {
-    m_width = (double) value;
+    m_width = value;
     emit changed();
   }
 
@@ -179,7 +179,7 @@ namespace Avogadro {
   void HBondEngine::readSettings(QSettings &settings)
   {
     Engine::readSettings(settings);
-    setWidth(settings.value("width", 2.0).toDouble());
+    setWidth(settings.value("width", 2).toInt());
     setRadius(settings.value("radius", 2.0).toDouble());
     setAngle(settings.value("angle", 120.0).toDouble());
     if (m_settingsWidget) {

libavogadro/src/engines/hbondengine.h
@@ -73,7 +73,7 @@ namespace Avogadro {
 
     private:
       HBondSettingsWidget *m_settingsWidget;
-      double m_width;
+      int    m_width;
       double m_radius;
       double m_angle;
   

libavogadro/src/navigate.cpp
@@ -66,7 +66,7 @@ namespace Avogadro {
                            double deltaX, double deltaY)
   {
     Vector3d fromPos = widget->camera()->unProject(QPoint(0, 0), what);
-    Vector3d toPos = widget->camera()->unProject(QPoint(deltaX, deltaY), what);
+    Vector3d toPos = widget->camera()->unProject(QPoint(int(deltaX), int(deltaY)), what);
     widget->camera()->translate(toPos - fromPos);
   }
 

libavogadro/src/neighborlist.cpp
@@ -174,9 +174,9 @@ namespace Avogadro
     }
 
     // set the dimentions
-    m_dim.x() = floor( (m_max.x() - m_min.x()) /  m_edgeLength) + 1;
-    m_dim.y() = floor( (m_max.y() - m_min.y()) /  m_edgeLength) + 1;
-    m_dim.z() = floor( (m_max.z() - m_min.z()) /  m_edgeLength) + 1;
+    m_dim.x() = int(floor( (m_max.x() - m_min.x()) /  m_edgeLength)) + 1;
+    m_dim.y() = int(floor( (m_max.y() - m_min.y()) /  m_edgeLength)) + 1;
+    m_dim.z() = int(floor( (m_max.z() - m_min.z()) /  m_edgeLength)) + 1;
     m_xyDim = m_dim.x() * m_dim.y();
 
     updateCells();

libavogadro/src/neighborlist.h
@@ -122,17 +122,17 @@ namespace Avogadro
 
       inline unsigned int cellIndex(const Eigen::Vector3d &pos) const
       {
-        return cellIndex( floor( (pos.x() - m_min.x()) / m_edgeLength ),
-                          floor( (pos.y() - m_min.y()) / m_edgeLength ),
-                          floor( (pos.z() - m_min.z()) / m_edgeLength ) );
+        return cellIndex( int(floor( (pos.x() - m_min.x()) / m_edgeLength )),
+                          int(floor( (pos.y() - m_min.y()) / m_edgeLength )),
+                          int(floor( (pos.z() - m_min.z()) / m_edgeLength )) );
       }
 
       inline Eigen::Vector3i cellIndexes(const Eigen::Vector3d *pos) const
       {
         Eigen::Vector3i index;
-        index.x() = floor( (pos->x() - m_min.x()) / m_edgeLength );
-        index.y() = floor( (pos->y() - m_min.y()) / m_edgeLength );
-        index.z() = floor( (pos->z() - m_min.z()) / m_edgeLength );
+        index.x() = int(floor( (pos->x() - m_min.x()) / m_edgeLength ));
+        index.y() = int(floor( (pos->y() - m_min.y()) / m_edgeLength ));
+        index.z() = int(floor( (pos->z() - m_min.z()) / m_edgeLength ));
         return index;
       }
 
@@ -185,7 +185,7 @@ namespace Avogadro
 
       Eigen::Vector3d                     m_min, m_max;
       Eigen::Vector3i                     m_dim;
-      double                              m_xyDim;
+      int                                 m_xyDim;
       std::vector<std::vector<Atom*> >    m_cells;
 
       std::vector<Eigen::Vector3i>        m_offsetMap;

commit 1f6d44d362c023f73348fee7771e6dbf828b287e

Fixed bug introduced by krazy fixing - broken isosurfaces.
Commit 4678b8d53bab0e5104515276da0332fdb619dc74 introduced a bug where
isosurfaces were broken. Minor fix to bracket the difference between
val1 and val2. We need more unit tests to catch problems like this - I
will make it a priority!

libavogadro/src/meshgenerator.cpp
@@ -140,7 +140,7 @@ namespace Avogadro {
 
   inline float MeshGenerator::offset(float val1, float val2)
   {
-    if (val2 - val1 < 1.0e-5f)
+    if (val2 - val1 < 1.0e-9f && val1 - val2 < 1.0e-9f)
       return 0.5;
     return (m_iso - val1) / (val2 - val1);
   }