516b046249a3340281c0b569e0568ed2a3a6068e
Geoff Hutchison <geoff@geoffhutchison.net>
2009-06-08 18:23:16 -0400

    Switch to use PO files and lconvert for building translations. (Ideally, avogadro/src/CMakeLists.txt and libavogadro/src/CMakeLists.txt    won't wrap TS files until translations are built.) CMake help here would be appreciated. Right now, it's a race condition.

--- scripts/create-translations.sh
+++ scripts/create-translations.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
-for lang in `ls avogadro*.po | cut -f 2 -d '-' | cut -f 1 -d '.'`; do
-  lconvert -i avogadro-${lang}.po -o avogadro_${lang}.ts;
-done
+sourcedir=${1-.}
+lconvert=${2}
 
-for lang in `ls libavogadro*.po | cut -f 2 -d '-' | cut -f 1 -d '.'`; do
-  lconvert -i libavogadro-${lang}.po -o libavogadro_${lang}.ts;
-done
+#if [ x"${lconvert}" != "x" ]; then
+
+    for lang in `ls ${sourcedir}/avogadro*.po | cut -f 2 -d '-' | cut -f 1 -d '.'`; do
+        ${lconvert} -i ${sourcedir}/avogadro-${lang}.po -o avogadro_${lang}.ts;
+    done
+
+    for lang in `ls ${sourcedir}/libavogadro*.po | cut -f 2 -d '-' | cut -f 1 -d '.'`; do
+        ${lconvert} -i ${sourcedir}/libavogadro-${lang}.po -o libavogadro_${lang}.ts;
+    done
+
+#fi
