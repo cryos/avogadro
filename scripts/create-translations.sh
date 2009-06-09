79c052eff51a310f07ac5608b002372ea8395bb8
Geoff Hutchison <geoff@geoffhutchison.net>
2009-06-08 21:10:13 -0400

    Probably the last PO translation update. Seems to work smoothly now. I *HATE* add_custom_target -- it re-runs all the time. If someone knows Cmake magic, let's fix this.

--- scripts/create-translations.sh
+++ scripts/create-translations.sh
@@ -6,11 +6,11 @@ lconvert=${2}
 #if [ x"${lconvert}" != "x" ]; then
 
     for lang in `ls ${sourcedir}/avogadro*.po | cut -f 2 -d '-' | cut -f 1 -d '.'`; do
-        ${lconvert} -i ${sourcedir}/avogadro-${lang}.po -o avogadro_${lang}.ts;
+        ${lconvert} -i ${sourcedir}/avogadro-${lang}.po -o ./avogadro_${lang}.ts;
     done
 
     for lang in `ls ${sourcedir}/libavogadro*.po | cut -f 2 -d '-' | cut -f 1 -d '.'`; do
-        ${lconvert} -i ${sourcedir}/libavogadro-${lang}.po -o libavogadro_${lang}.ts;
+        ${lconvert} -i ${sourcedir}/libavogadro-${lang}.po -o ./libavogadro_${lang}.ts;
     done
 
 #fi
