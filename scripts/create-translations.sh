f12eb0dd56dcd34cd5b3c8ab385f1237cd9e4c03
Tim Vandermeersch <tim.vandermeersch@gmail.com>
2009-06-10 22:45:43 +0200

    Improve translation creation & updating. The replacing of "-" with "_" caused some problems here since my build directory contains a "-" character.

--- scripts/create-translations.sh
+++ scripts/create-translations.sh
@@ -5,12 +5,16 @@ lconvert=${2}
 
 #if [ x"${lconvert}" != "x" ]; then
 
-    for lang in `ls ${sourcedir}/avogadro*.po | cut -f 2 -d '-' | cut -f 1 -d '.'`; do
+    for filepath in `ls ${sourcedir}/avogadro*.po`; do
+        filename=`basename ${filepath}`
+        lang=`echo ${filename} | cut -f 2 -d '-' | cut -f 1 -d '.'`
         ${lconvert} -i ${sourcedir}/avogadro-${lang}.po -o ./avogadro_${lang}.ts;
     done
 
-    for lang in `ls ${sourcedir}/libavogadro*.po | cut -f 2 -d '-' | cut -f 1 -d '.'`; do
-        ${lconvert} -i ${sourcedir}/libavogadro-${lang}.po -o ./libavogadro_${lang}.ts;
-    done
+    for filepath in `ls ${sourcedir}/libavogadro*.po`; do
+       filename=`basename ${filepath}`
+       lang=`echo ${filename} | cut -f 2 -d '-' | cut -f 1 -d '.'`
+       ${lconvert} -i ${sourcedir}/libavogadro-${lang}.po -o ./libavogadro_${lang}.ts;
+   done
 
 #fi
