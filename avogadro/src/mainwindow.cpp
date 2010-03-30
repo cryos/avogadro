40990007668a76ad4382ba805de65c8f937ce991
Geoff Hutchison <geoff@geoffhutchison.net>
2010-03-29 21:20:38 -0400

    Fix -- check for default filename on Mac before deciding to load in new window.

--- avogadro/src/mainwindow.cpp
+++ avogadro/src/mainwindow.cpp
@@ -759,7 +759,7 @@ namespace Avogadro
       // if we have nothing open or modified
       bool loadInNewWindow = isWindowModified();
 #ifdef Q_WS_MAC
-      loadInNewWindow = true; // always load into a new window on Mac PR#2945916
+      loadInNewWindow = !isDefaultFileName(d->fileName); // always load into a new window on Mac PR#2945916
 #endif
 
       if ( !loadInNewWindow ) {
