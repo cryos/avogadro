86ced3e8b10f7ecbc48728a3680472d6b66b94d7
Geoff Hutchison <geoff@geoffhutchison.net>
2010-09-04 12:28:16 -0400

    Don't extract strings from "example" code.
    Change-Id: I05c60b6842e39c345f029fc7b30f012eb01e5d68

--- scripts/extract-library-messages.sh
+++ scripts/extract-library-messages.sh
@@ -26,7 +26,7 @@ echo "Done preparing rc files"
 echo "Extracting messages"
 cd ${BASEDIR}
 # see above on sorting
-find . -name '*.cpp' -o -name '*.h' -o -name '*.c' | sort > ${WDIR}/infiles.list
+find . -name '*.cpp' -o -name '*.h' -o -name '*.c' | grep -v "example" | sort > ${WDIR}/infiles.list
 echo "rc.cpp" >> ${WDIR}/infiles.list
 cd ${WDIR}
 xgettext --from-code=UTF-8 -C -kde -ci18n -ki18n:1 -ki18nc:1c,2 -ki18np:1,2 -ki18ncp:1c,2,3 -ktr2i18n:1 \
