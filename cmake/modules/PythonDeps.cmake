7269a298b12b337d604615134dc0c1b62068657a
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2011-01-21 13:40:20 -0500

    Backported fixes to PythonDeps.cmake from master.
    Change-Id: Id3fccf8a9a7c8fd41782192a0fbd9490f0588748

--- cmake/modules/PythonDeps.cmake
+++ cmake/modules/PythonDeps.cmake
@@ -4,7 +4,7 @@
 
 if(Boost_PYTHON_FOUND AND PYTHONLIBS_FOUND AND NUMPY_FOUND)
   # In cache already
-  set(ALL_PYTHON_FOUND TRUE CACHE BOOL "True if all Python dependencies satisfied.")
+  set(ALL_PYTHON_FOUND TRUE)
 
 else(Boost_PYTHON_FOUND AND PYTHONLIBS_FOUND AND NUMPY_FOUND)
 
@@ -13,14 +13,14 @@ else(Boost_PYTHON_FOUND AND PYTHONLIBS_FOUND AND NUMPY_FOUND)
   # Boost Python
   message(STATUS "[1/5] Boost Python")
   # CMake's new FindBoost has an option to look for additional versions
-  set(Boost_ADDITIONAL_VERSIONS "1.40" "1.40.0" "1.39" "1.39.0" "1.38" "1.38.0" "1.37" "1.37.0")
+  set(Boost_ADDITIONAL_VERSIONS "1.45" "1.44" "1.43" "1.42" "1.41" "1.40" "1.40.0" "1.39" "1.39.0" "1.38" "1.38.0" "1.37" "1.37.0")
   find_package(Boost COMPONENTS python)
   if (Boost_PYTHON_FOUND)
     message(STATUS "Boost Python found...")
   else (Boost_PYTHON_FOUND)
     message(STATUS "Boost Python NOT found - Python support disabled.")
   #  message(STATUS "debian/ubuntu: install the libboost-python-dev package.")
-    set(ALL_PYTHON_FOUND FALSE CACHE BOOL "True if all Python dependencies satisfied.")
+    set(ALL_PYTHON_FOUND FALSE)
     return()
   endif (Boost_PYTHON_FOUND)
 
@@ -30,7 +30,7 @@ else(Boost_PYTHON_FOUND AND PYTHONLIBS_FOUND AND NUMPY_FOUND)
   if (NOT PYTHONLIBS_FOUND)
     message(STATUS "Python libraries NOT found - Python support disabled.")
   #  message(STATUS "debian/ubuntu: install the python-dev package. Correct?????")
-    set(ALL_PYTHON_FOUND FALSE CACHE BOOL "True if all Python dependencies satisfied.")
+    set(ALL_PYTHON_FOUND FALSE)
     return()
   endif (NOT PYTHONLIBS_FOUND)
 
@@ -40,7 +40,7 @@ else(Boost_PYTHON_FOUND AND PYTHONLIBS_FOUND AND NUMPY_FOUND)
   if (NOT PYTHONINTERP_FOUND)
     message(STATUS "Python interpreter NOT found - Python support disabled.")
   #  message(STATUS "debian/ubuntu: install the python package.")
-    set(ALL_PYTHON_FOUND FALSE CACHE BOOL "True if all Python dependencies satisfied.")
+    set(ALL_PYTHON_FOUND FALSE)
     return()
   endif (NOT PYTHONINTERP_FOUND)
 
@@ -50,7 +50,7 @@ else(Boost_PYTHON_FOUND AND PYTHONLIBS_FOUND AND NUMPY_FOUND)
   if (NOT NUMPY_FOUND)
     message(STATUS "Numpy NOT found - Python support disabled.")
   #  message(STATUS "debian/ubuntu: install the python-numpy package.")
-    set(ALL_PYTHON_FOUND FALSE CACHE BOOL "True if all Python dependencies satisfied.")
+    set(ALL_PYTHON_FOUND FALSE)
     return()
   endif (NOT NUMPY_FOUND)
 
@@ -58,15 +58,16 @@ else(Boost_PYTHON_FOUND AND PYTHONLIBS_FOUND AND NUMPY_FOUND)
   message(STATUS "[5/5] SIP Module")
   if (EXISTS ${PYTHON_INCLUDE_PATH}/sip.h)
     message(STATUS "Found sip.h header...")
-    execute_process(COMMAND sip -V OUTPUT_VARIABLE SIP_VERSION)
-    if (${SIP_VERSION} VERSION_GREATER "4.8")
+    find_file(SIP_EXECUTABLE sip)
+    execute_process(COMMAND "${SIP_EXECUTABLE}" -V OUTPUT_VARIABLE SIP_VERSION)
+    if ("${SIP_VERSION}" VERSION_GREATER "4.8")
       message(STATUS "using sip version 4.8 or above...")
       add_definitions( -DSIP_4_8 )
-    endif (${SIP_VERSION} VERSION_GREATER "4.8")
+    endif ("${SIP_VERSION}" VERSION_GREATER "4.8")
   else (EXISTS ${PYTHON_INCLUDE_PATH}/sip.h)
     message(STATUS "sip.h header NOT found - Python support disabled")
   #  message(STATUS "debian/ubuntu: install the python-sip4-dev package.")
-    set(ALL_PYTHON_FOUND FALSE CACHE BOOL "True if all Python dependencies satisfied.")
+    set(ALL_PYTHON_FOUND FALSE)
     return()
   endif (EXISTS ${PYTHON_INCLUDE_PATH}/sip.h)
 
@@ -77,8 +78,6 @@ else(Boost_PYTHON_FOUND AND PYTHONLIBS_FOUND AND NUMPY_FOUND)
   # #endif
   add_definitions( -DENABLE_PYTHON )
   # to be used in subdir CMakeLists.txt files
-  set(ALL_PYTHON_FOUND TRUE CACHE BOOL "True if all Python dependencies satisfied.")
+  set(ALL_PYTHON_FOUND TRUE)
 
 endif(Boost_PYTHON_FOUND AND PYTHONLIBS_FOUND AND NUMPY_FOUND)
-
-mark_as_advanced(ALL_PYTHON_FOUND)
