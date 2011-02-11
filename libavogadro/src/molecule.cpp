ef88b9ad50f4ee7cda8fd46e9018d8638831d19b
Konstantin Tokarev <annulen@yandex.ru>
2011-02-11 17:54:40 +0300

    Prevent crash in setDipoleMoment and related memory leak in setOBMol
    Change-Id: I5c3bf916d21d1c82fc6a952d7fe56953a8f1cd0d

--- libavogadro/src/molecule.cpp
+++ libavogadro/src/molecule.cpp
@@ -704,7 +704,10 @@ namespace Avogadro{
 
   void Molecule::setDipoleMoment(const Eigen::Vector3d &moment)
   {
-    *m_dipoleMoment = moment;
+    if(!m_dipoleMoment)
+      m_dipoleMoment = new Vector3d(moment);
+    else
+      *m_dipoleMoment = moment;
     m_estimatedDipoleMoment = false;
   }
 
@@ -1292,7 +1295,10 @@ namespace Avogadro{
     OpenBabel::OBVectorData *vd = (OpenBabel::OBVectorData*)obmol->GetData("Dipole Moment");
     if (vd) {
       OpenBabel::vector3 moment = vd->GetData();
-      m_dipoleMoment = new Vector3d(moment.x(), moment.y(), moment.z());
+      if (!m_dipoleMoment)
+        m_dipoleMoment = new Vector3d(moment.x(), moment.y(), moment.z());
+      else
+        *m_dipoleMoment = Vector3d(moment.x(), moment.y(), moment.z());
       m_estimatedDipoleMoment = false;
     }
 
