2d0a0d38403b66a36075cd8ac69582e13bf08565
Mickaël Gadroy <mickael.gadroy@gmail.com>
2011-02-16 11:35:09 +0100

    Add plugin directories ... Re-test to push ...

---
+++ pluginadditionnal/myex_ex/v1_cmake_avo/myextension.cpp
@@ -0,0 +1,56 @@
+
+#include "myextension.h"
+#include <avogadro/glwidget.h>
+#include <avogadro/molecule.h>
+#include <avogadro/atom.h>
+#include <avogadro/primitivelist.h>
+
+#include <openbabel/mol.h>
+#include <openbabel/obiter.h>
+
+#include <QAction>
+
+using namespace std;
+using namespace OpenBabel;
+
+namespace Avogadro {
+
+  MyExtension::MyExtension(QObject *parent) : Extension(parent),
+    m_molecule(0)
+  {
+    QAction *action = new QAction(this);
+    action->setText(tr("Nothing to nothing"));
+    m_actions.append(action);
+
+    action = new QAction( this );
+    action->setSeparator(true);
+    m_actions.append( action );
+  }
+
+  MyExtension::~MyExtension()
+  {
+  }
+
+  QList<QAction *> MyExtension::actions() const
+  {
+    return m_actions;
+  }
+
+  QString MyExtension::menuPath(QAction *) const
+  {
+    return tr("&Construction");
+  }
+
+  QUndoCommand* MyExtension::performAction( QAction *, GLWidget *widget )
+  {
+    QUndoCommand *undo = 0;
+    return undo;
+  }
+
+
+} // end namespace Avogadro
+
+#include "myextension.moc"
+
+Q_EXPORT_PLUGIN2(MyExtension, Avogadro::MyExtensionFactory)
+
