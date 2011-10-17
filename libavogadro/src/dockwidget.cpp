6979f881abcaf6b2412a0c6a8f5f4080e326ca61
David C. Lonie <loniedavid@gmail.com>
2011-10-17 19:04:37 -0400

    Add the DockWidget class to wrap QDockWidget.
    This will eventually simplify the extension hierarchy by removing the
    DockExtension class (Extension already had a dockWidget() method) and
    adding the "preferredDockWidgetArea" property to the widget itself.
    
    This allows extensions to have as many dockwidgets as needed and
    individually specify their locations.
    
    Change-Id: I8148e9def8416cd5c1c611b4f1aec61d049d4cc1

---
+++ libavogadro/src/dockwidget.cpp
@@ -0,0 +1,50 @@
+/**********************************************************************
+  DockWidget -- Dock widget for avogadro
+
+  Copyright (C) 2011 David C. Lonie
+
+  This file is part of the Avogadro molecular editor project.
+  For more information, see <http://avogadro.openmolecules.net/>
+
+  This source code is released under the New BSD License, (the "License").
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ **********************************************************************/
+
+#include "dockwidget.h"
+
+namespace Avogadro
+{
+
+DockWidget::DockWidget(const QString &title,
+                       QWidget *parent, Qt::WindowFlags flags)
+  : QDockWidget(title, parent, flags),
+    m_preferredDockWidgetArea(Qt::RightDockWidgetArea)
+{
+}
+
+DockWidget::DockWidget(QWidget *parent, Qt::WindowFlags flags)
+  : QDockWidget(parent, flags),
+    m_preferredDockWidgetArea(Qt::RightDockWidgetArea)
+{
+}
+
+DockWidget::~DockWidget()
+{
+}
+
+Qt::DockWidgetArea DockWidget::preferredWidgetDockArea()
+{
+  return this->m_preferredDockWidgetArea;
+}
+
+void DockWidget::setPreferredDockWidgetArea(Qt::DockWidgetArea pref)
+{
+  this->m_preferredDockWidgetArea = pref;
+}
+
+}
