65fc5d0da002c038b8b82f98ff48e185fa00b6ce
David C. Lonie <loniedavid@gmail.com>
2011-10-17 19:06:57 -0400

    Add a nanotube builder that uses the TubeGen code[1] (BSD license).
    [1] http://turin.nss.udel.edu/research/tubegenonline.html
    
    Change-Id: I2b2c97498b4a973cc20c3ea69fe8926137d3cbcf

---
+++ libavogadro/src/extensions/swcntbuilder/swcntbuilderwidget.cpp
@@ -0,0 +1,149 @@
+/**********************************************************************
+  SWCNTBuilderDialog - Build single wall carbon nanotubes
+
+  Copyright (C) 2011 David C. Lonie
+
+  This file is part of the Avogadro molecular editor project.
+  For more information, see <http://avogadro.openmolecules.net/>
+
+  This source code is released under the New BSD License, (the "License").
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ **********************************************************************/
+
+#include "swcntbuilderwidget.h"
+
+#include <avogadro/dockwidget.h>
+
+#include <QtCore/QSettings>
+#include <QtGui/QApplication>
+
+#include <Eigen/Core>
+
+using namespace Avogadro;
+
+namespace SWCNTBuilder {
+
+  SWCNTBuilderWidget::SWCNTBuilderWidget(const QString &title,
+                                         QWidget *parent, Qt::WindowFlags f )
+    : DockWidget( title, parent, f )
+  {
+    ctor();
+  }
+
+  SWCNTBuilderWidget::SWCNTBuilderWidget(QWidget *parent, Qt::WindowFlags f)
+    : DockWidget (parent, f)
+  {
+    ctor();
+  }
+
+  SWCNTBuilderWidget::~SWCNTBuilderWidget()
+  {
+    this->writeSettings();
+  }
+
+  void SWCNTBuilderWidget::ctor()
+  {
+    ui.setupUi(this);
+
+    this->readSettings();
+
+    this->connect(ui.push_build, SIGNAL(clicked()), SIGNAL(buildClicked()));
+    this->connect(ui.push_hide, SIGNAL(clicked()), SLOT(close()));
+  }
+
+  unsigned int SWCNTBuilderWidget::getN()
+  {
+    return static_cast<unsigned int>(ui.spin_n->value());
+  }
+
+  unsigned int SWCNTBuilderWidget::getM()
+  {
+    return static_cast<unsigned int>(ui.spin_m->value());
+  }
+
+  double SWCNTBuilderWidget::getLength()
+  {
+    return ui.spin_length->value();
+  }
+
+  SWCNTBuilderWidget::LengthUnit SWCNTBuilderWidget::getLengthUnit()
+  {
+    return static_cast<SWCNTBuilderWidget::LengthUnit>(
+          ui.combo_lengthUnit->currentIndex());
+  }
+
+  bool SWCNTBuilderWidget::getCap()
+  {
+    return ui.cb_cap->isChecked();
+  }
+
+  bool SWCNTBuilderWidget::getDBonds()
+  {
+    return ui.cb_dbonds->isChecked();
+  }
+
+  bool SWCNTBuilderWidget::getAutohide()
+  {
+    return ui.push_autohide->isChecked();
+  }
+
+  void SWCNTBuilderWidget::startBuilding()
+  {
+    ui.push_build->setText(tr("Building..."));
+    ui.push_build->setDisabled(true);
+    qApp->setOverrideCursor(Qt::WaitCursor);
+
+    ui.gb_nm->setDisabled(true);
+    ui.gb_length->setDisabled(true);
+    ui.gb_options->setDisabled(true);
+  }
+
+  void SWCNTBuilderWidget::endBuilding()
+  {
+    ui.push_build->setText(tr("&Build"));
+    ui.push_build->setEnabled(true);
+    qApp->restoreOverrideCursor();
+
+    ui.gb_nm->setEnabled(true);
+    ui.gb_length->setEnabled(true);
+    ui.gb_options->setEnabled(true);
+  }
+
+  void SWCNTBuilderWidget::writeSettings()
+  {
+    QSettings settings;
+    settings.beginGroup("swcntbuilderextension");
+
+    settings.setValue("n", this->getN());
+    settings.setValue("m", this->getM());
+    settings.setValue("length", this->getLength());
+    settings.setValue("lengthUnit", this->getLengthUnit());
+    settings.setValue("cap", this->getCap());
+    settings.setValue("dbonds", this->getDBonds());
+    settings.setValue("autohide", this->getAutohide());
+
+    settings.endGroup();
+  }
+
+  void SWCNTBuilderWidget::readSettings()
+  {
+    QSettings settings;
+    settings.beginGroup("swcntbuilderextension");
+
+    ui.spin_n->setValue(settings.value("n", 0).toInt());
+    ui.spin_m->setValue(settings.value("m", 4).toInt());
+    ui.spin_length->setValue(settings.value("length", 1.0).toDouble());
+    ui.combo_lengthUnit->setCurrentIndex(
+          settings.value("lengthUnit", LU_Periodic).toInt());
+    ui.cb_cap->setChecked(settings.value("cap", true).toBool());
+    ui.cb_dbonds->setChecked(settings.value("dbonds", false).toBool());
+    ui.push_autohide->setChecked(settings.value("autohide", true).toBool());
+
+    settings.endGroup();
+  }
+}
---
+++ libavogadro/src/extensions/swcntbuilder/tubegen/ANSR.h
@@ -0,0 +1,122 @@
+// =============================================================
+// ANSR.h																	 �1999-2002, J.T. Frey
+// =============================================================
+// Written:		J.T. Frey, 23.FEB.2002
+// Purpose:		Atomic Number/Symbol Resolution class.
+//
+// Last Mod:	05.Feb.2003:	Removed all ostream::form calls and moved to purely
+//													C++ stream operators.
+
+#ifndef __ANSRDB__
+#define __ANSRDB__
+
+// Removed for Avogadro:
+// #include <stdcxx-includes.h>
+//
+// Replaced with:
+#include <iostream>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
+
+//
+
+	/////////////////////////////////////////////////////////
+	// TElementSymbol - four-character entity, masquerading
+	// as a simple 32-bit integer!  For any symbol/number
+	// that we can't resolve, there's a constant, too:
+	//
+	typedef			uint32_t							TElementSymbol;
+	
+	#define     kANSRInvalidSymbol		(TElementSymbol)-1
+	
+//
+
+	/////////////////////////////////////////////////////////
+	// TElementInfo - data structure which contains an
+	// element's information.
+	//
+	typedef struct TElementInfo {
+		unsigned							atomicNumber;
+	  TElementSymbol				chemSymbol;
+		double								weight;
+	} TElementInfo;
+	
+//
+
+	/////////////////////////////////////////////////////////
+	// ANSRDB - Atomic Number/Symbol Resolution Data Base
+	//
+	class ANSRDB {
+	  public:
+			ANSRDB();
+			ANSRDB(const char* filepath);
+			~ANSRDB();
+      
+      static ANSRDB* DefaultANSRDB()
+        {
+          extern char* defaultANSRDBPath;
+          static ANSRDB* defaultANSRDBInstance = NULL;
+          
+          if (!defaultANSRDBInstance) {
+            FILE*     fptr;
+            
+            if (fptr = fopen(defaultANSRDBPath,"r")) {
+              fclose(fptr);
+              defaultANSRDBInstance = new ANSRDB(defaultANSRDBPath);
+              if (defaultANSRDBInstance->elementsInTable == 0) {
+                delete defaultANSRDBInstance;
+                defaultANSRDBInstance = NULL;
+              }
+            }
+            if (!defaultANSRDBInstance)
+              defaultANSRDBInstance = new ANSRDB();
+          }
+          return defaultANSRDBInstance;
+        }
+			
+			static TElementSymbol MakeSymbolFromString(const char* string)
+        {
+          TElementSymbol	newSym = 0;
+          unsigned        length = strlen(string);
+        
+          if (length) {
+            char*         chars = (char*)&newSym;
+            
+            if (length > 3) length = 3;
+            switch (length) {
+              case 3:
+                if (string[2] < 127 && isalpha(string[2]))
+                  chars[2] = tolower(string[2]);
+              case 2:
+                if (string[1] < 127 && isalpha(string[1]))
+                  chars[1] = tolower(string[1]);
+              case 1:
+                if (string[0] < 127 && isalpha(string[0]))
+                  chars[0] = toupper(string[0]);
+            }
+          }
+          return newSym;
+        }
+			
+			TElementSymbol LookupSymbolForNumber(unsigned number);
+			unsigned LookupNumberForSymbol(TElementSymbol symbol);
+			TElementInfo* LookupElementInfoForNumber(unsigned number);
+			TElementInfo* LookupElementInfoForSymbol(TElementSymbol symbol);
+			
+			void print(ostream& os);
+			
+		protected:
+		  unsigned							elementsInTable;
+		  TElementInfo*					elements;
+		  unsigned*							lookupTable;
+			
+		private:
+			int DidInitializeTables();
+			int DidReadTableFromFile(const char* filepath);
+	};
+
+#endif
