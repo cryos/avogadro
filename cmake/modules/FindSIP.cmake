43f07183122b87be5b6cb2c011b6c615d585acd3
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2012-04-12 14:53:08 -0400

    Style changes - use modern CMake style
    Change-Id: I145f76e9820c07535b5cda3ef6a2bc7783c071a3

--- cmake/modules/FindSIP.cmake
+++ cmake/modules/FindSIP.cmake
@@ -5,33 +5,33 @@
 #  SIP_FOUND, If false, do not try to use numpy headers.
 
 # be certain sip is available from python.  this part might be unnecessary.
-EXECUTE_PROCESS(COMMAND ${PYTHON_EXECUTABLE} -c
-    "import sip; print sip.SIP_VERSION"
-    OUTPUT_VARIABLE SIP_VERSION
-    OUTPUT_STRIP_TRAILING_WHITESPACE)
+execute_process(COMMAND ${PYTHON_EXECUTABLE} -c
+  "import sip; print sip.SIP_VERSION"
+  OUTPUT_VARIABLE SIP_VERSION
+  OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-if (SIP_VERSION)
+if(SIP_VERSION)
   set (SIP_PYTHON_FOUND TRUE)
-else (SIP_VERSION)
+else()
   message(STATUS "SIP module not available in Python." )
   set (SIP_PYTHON_FOUND FALSE)
-endif (SIP_VERSION)
+endif()
 
-if (SIP_PYTHON_FOUND)
+if(SIP_PYTHON_FOUND)
   # search for sip.h
-  if (SIP_INCLUDE_DIR)
-    set (SIP_FOUND TRUE)
-  else (SIP_INCLUDE_DIR)
+  if(SIP_INCLUDE_DIR)
+    set(SIP_FOUND TRUE)
+  else()
     find_path(SIP_INCLUDE_DIR NAMES sip.h
      PATHS
      PATH_SUFFIXES SIP
     )
-  endif (SIP_INCLUDE_DIR)
+  endif()
   include(FindPackageHandleStandardArgs)
   find_package_handle_standard_args(SIP DEFAULT_MSG SIP_INCLUDE_DIR )
-else (SIP_PYTHON_FOUND)
+else()
   # give up
-  set (SIP_FOUND FALSE)
-endif (SIP_PYTHON_FOUND)
+  set(SIP_FOUND FALSE)
+endif()
 
-MARK_AS_ADVANCED (SIP_INCLUDE_DIR)
+mark_as_advanced(SIP_INCLUDE_DIR)
