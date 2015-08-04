bf59672d2aae1c2e24984ee9c155eb369e1aae95
Geoff Hutchison <geoff.hutchison@gmail.com>
2015-08-04 15:39:12 -0400

    Fix Avogadro homepage (http://avogadro.cc/)
    Change-Id: I805627e1ab8986d14cbf7af117661d5fe2225cc6

--- libavogadro/src/extensions/orca/orcaextension.h
+++ libavogadro/src/extensions/orca/orcaextension.h
@@ -5,7 +5,7 @@
   Some portions Copyright (C) 2014 Dagmar lenk
 
   This file is part of the Avogadro molecular editor project.
-  For more information, see <http://avogadro.openmolecules.net/>
+  For more information, see <http://avogadro.cc/>
 
   Avogadro is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
